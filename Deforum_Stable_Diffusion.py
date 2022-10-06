0a54ef41ad8f8f53678603490650218e2d15d330
deforum <110359942+deforum@users.noreply.github.com>
2022-10-06 16:04:47 -0700

    update env setup for colab

--- Deforum_Stable_Diffusion.py
+++ Deforum_Stable_Diffusion.py
@@ -108,30 +108,19 @@ if 'google.colab' in str(ipy):
         ['pip', 'install', 'torch==1.12.1+cu113', 'torchvision==0.13.1+cu113', '--extra-index-url', 'https://download.pytorch.org/whl/cu113'],
         ['pip', 'install', 'omegaconf==2.2.3', 'einops==0.4.1', 'pytorch-lightning==1.7.4', 'torchmetrics==0.9.3', 'torchtext==0.13.1', 'transformers==4.21.2', 'kornia==0.6.7'],
         ['git', 'clone',  '-b', 'local', 'https://github.com/deforum/stable-diffusion'],
-        ['pip', 'install', '-e', 'git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers'],
-        ['pip', 'install', '-e', 'git+https://github.com/openai/CLIP.git@main#egg=clip'],
         ['pip', 'install', 'accelerate', 'ftfy', 'jsonmerge', 'matplotlib', 'resize-right', 'timm', 'torchdiffeq'],
-        ['git', 'clone', 'https://github.com/shariqfarooq123/AdaBins.git'],
-        ['git', 'clone', 'https://github.com/isl-org/MiDaS.git'],
-        ['git', 'clone', 'https://github.com/MSFTserver/pytorch3d-lite.git'],
     ]
     for process in all_process:
         running = subprocess.run(process,stdout=subprocess.PIPE).stdout.decode('utf-8')
         if print_subprocess:
             print(running)
-    
-    print(subprocess.run(['git', 'clone', 'https://github.com/deforum/k-diffusion/'], stdout=subprocess.PIPE).stdout.decode('utf-8'))
-    with open('k-diffusion/k_diffusion/__init__.py', 'w') as f:
+
+    with open('stable-diffusion/src/k_diffusion/__init__.py', 'w') as f:
         f.write('')
 
     sys.path.extend([
-        'src/taming-transformers',
-        'src/clip',
         'stable-diffusion/',
-        'k-diffusion',
-        'pytorch3d-lite',
-        'AdaBins',
-        'MiDaS',
+        'stable-diffusion/src',
     ])
 
     end_time = time.time()
