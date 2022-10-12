ad14ab497af6862d2fa58c95ffe9525189f8404c
kabachuha <artemkhrapov2001@yandex.ru>
2022-10-13 01:16:36 +0300

    setup gui for anim args

--- deforum.py
+++ deforum.py
@@ -4,34 +4,115 @@ import gradio as gr
 from modules.processing import Processed
 from modules.shared import opts, cmd_opts, state
 
+import deforum.args as deforum_args
+
 class Script(scripts.Script):
 
     def title(self):
         return "Deforum"
 
     def ui(self, is_img2img):
-        code = gr.Textbox(label="Python code", visible=False, lines=1)
+        da = SimpleNamespace(**DeforumAnimArgs()) #default args
+        gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">Import settings from file</p>")
+        with gr.Row():
+            override_settings_with_file = gr.Checkbox(label="Override settings", value=False)
+            custom_settings_file = gr.Textbox(label="Custom settings file", lines=1)
+            
+            
+        gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">Animation settings</p>")
+        with gr.Row():
+            animation_mode = gr.Dropdown(label="animation_mode", choices=['None', '2D', '3D', 'Video Input', 'Interpolation'], value=da.animation_mode, type="index", elem_id="animation_mode")
+            max_frames = gr.Number(label="max_frames", value=da.max_frames)
+            border = gr.Dropdown(label="border", choices=['replicate', 'wrap'], value=da.border, type="index", elem_id="border")
+        
+        
+        gr.HTML("<p style=\"margin-bottom:0.75em\">Motion parameters:</p>")
+        gr.HTML("<p style=\"margin-bottom:0.75em\">2D and 3D settings</p>")
+        with gr.Row():
+            angle = gr.Textbox(label="angle", lines=1, value = da.angle)
+        with gr.Row():
+            zoom = gr.Textbox(label="zoom", lines=1, value = da.zoom)
+        with gr.Row():
+            translation_x = gr.Textbox(label="translation_x", lines=1, value = da.translation_x)
+        with gr.Row():
+            translation_y = gr.Textbox(label="translation_y", lines=1, value = da.translation_y)
+        gr.HTML("<p style=\"margin-bottom:0.75em\">3D settings</p>")
+        with gr.Row():
+            translation_z = gr.Textbox(label="translation_z", lines=1, value = da.translation_z)
+        with gr.Row():
+            rotation_3d_x = gr.Textbox(label="rotation_3d_x", lines=1, value = da.rotation_3d_x)
+        with gr.Row():
+            rotation_3d_y = gr.Textbox(label="rotation_3d_y", lines=1, value = da.rotation_3d_y)
+        with gr.Row():
+            rotation_3d_z = gr.Textbox(label="rotation_3d_z", lines=1, value = da.rotation_3d_z)
+        gr.HTML("<p style=\"margin-bottom:0.75em\">Prespective flip — Low VRAM pseudo-3D mode:</p>")
+        with gr.Row():
+            flip_2d_perspective = gr.Checkbox(label="flip_2d_perspective", value=da.flip_2d_perspective)
+        with gr.Row():
+            perspective_flip_theta = gr.Textbox(label="perspective_flip_theta", lines=1, value = da.perspective_flip_theta)
+        with gr.Row():
+            perspective_flip_phi = gr.Textbox(label="perspective_flip_phi", lines=1, value = da.perspective_flip_phi)
+        with gr.Row():
+            perspective_flip_gamma = gr.Textbox(label="perspective_flip_gamma", lines=1, value = da.perspective_flip_gamma)
+        with gr.Row():
+            perspective_flip_fv = gr.Textbox(label="perspective_flip_fv", lines=1, value = da.perspective_flip_fv)
+        gr.HTML("<p style=\"margin-bottom:0.75em\">Generation settings:</p>")
+        with gr.Row():
+            noise_schedule = gr.Textbox(label="noise_schedule", lines=1, value = da.noise_schedule)
+        with gr.Row():
+            strength_schedule = gr.Textbox(label="strength_schedule", lines=1, value = da.strength_schedule)
+        with gr.Row():
+            contrast_schedule = gr.Textbox(label="contrast_schedule", lines=1, value = da.contrast_schedule)
+#TODO
+#        with gr.Row():
+#            seed_schedule = gr.Textbox(label="seed_schedule", lines=1, value = da.seed_schedule)
+#        with gr.Row():
+#            scale_schedule = gr.Textbox(label="scale_schedule", lines=1, value = da.scale_schedule)
+        
+        gr.HTML("<p style=\"margin-bottom:0.75em\">Coherence:</p>")
+        with gr.Row():
+            color_coherence = gr.Dropdown(label="color_coherence", choices=['None', 'Match Frame 0 HSV', 'Match Frame 0 LAB', 'Match Frame 0 RGB'], value=da.color_coherence, type="index", elem_id="color_coherence")
+            diffusion_cadence = gr.Slider(label="diffusion_cadence", minimum=1, maximum=8, step=1, value=1)
+            
+        gr.HTML("<p style=\"margin-bottom:0.75em\">3D Depth Warping:</p>")
+        with gr.Row():
+            save_depth_maps = gr.Checkbox(label="save_depth_maps", value=False)
+            midas_weight = gr.Number(label="midas_weight", value=da.midas_weight)
+            near_plane = gr.Number(label="near_plane", value=da.near_plane)
+            far_plane = gr.Number(label="far_plane", value=da.far_plane)
+            fov = gr.Number(label="fov", value=da.fov)
+            padding_mode = gr.Dropdown(label="padding_mode", choices=['border', 'reflection', 'zeros'], value=da.padding_mode, type="index", elem_id="padding_mode")
+            sampling_mode = gr.Dropdown(label="sampling_mode", choices=['bicubic', 'bilinear', 'nearest'], value=da.sampling_mode, type="index", elem_id="sampling_mode")
+            save_depth_maps = gr.Checkbox(label="save_depth_maps", value=da.save_depth_maps)
+        
+        gr.HTML("<p style=\"margin-bottom:0.75em\">Video Input:</p>")
+        with gr.Row():
+            video_init_path = gr.Textbox(label="video_init_path", lines=1, value = da.video_init_path)
+        with gr.Row():
+            extract_nth_frame = gr.Number(label="extract_nth_frame", value=da.extract_nth_frame)
+            overwrite_extracted_frames = gr.Checkbox(label="overwrite_extracted_frames", value=False)
+            use_mask_video = gr.Checkbox(label="use_mask_video", value=False)
+        with gr.Row():
+            video_mask_path = gr.Textbox(label="video_mask_path", lines=1, value = da.video_mask_path)
+        
+        gr.HTML("<p style=\"margin-bottom:0.75em\">Interpolation:</p>")
+        with gr.Row():
+            interpolate_key_frames = gr.Checkbox(label="interpolate_key_frames", value=da.interpolate_key_frames)
+            interpolate_x_frames = gr.Number(label="interpolate_x_frames", value=da.interpolate_x_frames)
+        
+        gr.HTML("<p style=\"margin-bottom:0.75em\">Resume animation:</p>")
+        with gr.Row():
+            resume_from_timestring = gr.Checkbox(label="resume_from_timestring", value=da.resume_from_timestring)
+            resume_timestring = gr.Textbox(label="resume_timestring", lines=1, value = da.resume_timestring)
+            
 
-        return [code]
+        return [interpolate_x_frames]
 
 
-    def run(self, p, code):
-        assert cmd_opts.allow_code, '--allow-code option must be enabled'
+    def run(self, p, interpolate_x_frames):
+        print('Hello, deforum!')
 
-        display_result_data = [[], -1, ""]
-
-        def display(imgs, s=display_result_data[1], i=display_result_data[2]):
-            display_result_data[0] = imgs
-            display_result_data[1] = s
-            display_result_data[2] = i
-
-        from types import ModuleType
-        compiled = compile(code, '', 'exec')
-        module = ModuleType("testmodule")
-        module.__dict__.update(globals())
-        module.p = p
-        module.display = display
-        exec(compiled, module.__dict__)
+        display_result_data = [""]
 
         return Processed(p, *display_result_data)
     
---
+++ deforum/args.py
@@ -0,0 +1,60 @@
+def DeforumAnimArgs():
+
+    #@markdown ####**Animation:**
+    animation_mode = 'None' #@param ['None', '2D', '3D', 'Video Input', 'Interpolation'] {type:'string'}
+    max_frames = 1000 #@param {type:"number"}
+    border = 'replicate' #@param ['wrap', 'replicate'] {type:'string'}
+
+    #@markdown ####**Motion Parameters:**
+    angle = "0:(0)"#@param {type:"string"}
+    zoom = "0:(1.04)"#@param {type:"string"}
+    translation_x = "0:(10*sin(2*3.14*t/10))"#@param {type:"string"}
+    translation_y = "0:(0)"#@param {type:"string"}
+    translation_z = "0:(10)"#@param {type:"string"}
+    rotation_3d_x = "0:(0)"#@param {type:"string"}
+    rotation_3d_y = "0:(0)"#@param {type:"string"}
+    rotation_3d_z = "0:(0)"#@param {type:"string"}
+    flip_2d_perspective = False #@param {type:"boolean"}
+    perspective_flip_theta = "0:(0)"#@param {type:"string"}
+    perspective_flip_phi = "0:(t%15)"#@param {type:"string"}
+    perspective_flip_gamma = "0:(0)"#@param {type:"string"}
+    perspective_flip_fv = "0:(53)"#@param {type:"string"}
+    noise_schedule = "0: (0.02)"#@param {type:"string"}
+    strength_schedule = "0: (0.65)"#@param {type:"string"}
+    contrast_schedule = "0: (1.0)"#@param {type:"string"}
+#TODO
+#    seed_schedule = "0: (-1)"
+#    scale_schedule = "0: (7)"
+
+    #@markdown ####**Coherence:**
+    color_coherence = 'Match Frame 0 LAB' #@param ['None', 'Match Frame 0 HSV', 'Match Frame 0 LAB', 'Match Frame 0 RGB'] {type:'string'}
+    diffusion_cadence = '1' #@param ['1','2','3','4','5','6','7','8'] {type:'string'}
+
+    #@markdown ####**3D Depth Warping:**
+    use_depth_warping = True #@param {type:"boolean"}
+    midas_weight = 0.3#@param {type:"number"}
+    near_plane = 200
+    far_plane = 10000
+    fov = 40#@param {type:"number"}
+    padding_mode = 'border'#@param ['border', 'reflection', 'zeros'] {type:'string'}
+    sampling_mode = 'bicubic'#@param ['bicubic', 'bilinear', 'nearest'] {type:'string'}
+    save_depth_maps = False #@param {type:"boolean"}
+
+    #@markdown ####**Video Input:**
+    video_init_path ='/content/video_in.mp4'#@param {type:"string"}
+    extract_nth_frame = 1#@param {type:"number"}
+    overwrite_extracted_frames = True #@param {type:"boolean"}
+    use_mask_video = False #@param {type:"boolean"}
+    video_mask_path ='/content/video_in.mp4'#@param {type:"string"}
+
+    #@markdown ####**Interpolation:**
+    interpolate_key_frames = False #@param {type:"boolean"}
+    interpolate_x_frames = 4 #@param {type:"number"}
+    
+    #@markdown ####**Resume Animation:**
+    resume_from_timestring = False #@param {type:"boolean"}
+    resume_timestring = "20220829210106" #@param {type:"string"}
+
+    return locals()
+    
+    
