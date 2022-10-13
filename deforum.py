bf32a1de454cf7d668d5e38d351e5baa6f2afeb0
kabachuha <artemkhrapov2001@yandex.ru>
2022-10-13 16:30:08 +0300

    add all infoblocks to run

--- deforum.py
+++ deforum.py
@@ -15,16 +15,16 @@ class Script(scripts.Script):
     def ui(self, is_img2img):
         d = SimpleNamespace(**deforum_args.DeforumArgs())
         da = SimpleNamespace(**deforum_args.DeforumAnimArgs()) #default args
-        gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">Deforum v0.5-webui-beta</p>")
-        gr.HTML("<p style=\"margin-bottom:0.75em\">Made by deforum.github.io</p>")
-        gr.HTML("<p style=\"margin-bottom:0.75em\">Original Deforum Github repo  github.com/deforum/stable-diffusion</p>")
-        gr.HTML("<p style=\"margin-bottom:0.75em\">This WIP fork for auto1111's webui github.com/kabachuha/stable-diffusion/tree/automatic1111-webui</p>")
-        gr.HTML("<p style=\"margin-bottom:0.75em\">Join the official Deforum Discord discord.gg/deforum to share your creations and suggestions</p>")
-        gr.HTML("<p style=\"margin-bottom:0.75em\">User guide for v0.5 docs.google.com/document/d/1pEobUknMFMkn8F5TMsv8qRzamXX_75BShMMXV8IFslI/edit</p>")
-        gr.HTML("<p style=\"margin-bottom:0.75em\">Math keyframing explanation docs.google.com/document/d/1pfW1PwbDIuW0cv-dnuyYj1UzPqe23BlSLTJsqazffXM/edit?usp=sharing</p>")
+        i1 = gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">Deforum v0.5-webui-beta</p>")
+        i2 = gr.HTML("<p style=\"margin-bottom:0.75em\">Made by deforum.github.io</p>")
+        i3 = gr.HTML("<p style=\"margin-bottom:0.75em\">Original Deforum Github repo  github.com/deforum/stable-diffusion</p>")
+        i4 = gr.HTML("<p style=\"margin-bottom:0.75em\">This WIP fork for auto1111's webui github.com/kabachuha/stable-diffusion/tree/automatic1111-webui</p>")
+        i5 = gr.HTML("<p style=\"margin-bottom:0.75em\">Join the official Deforum Discord discord.gg/deforum to share your creations and suggestions</p>")
+        i6 = gr.HTML("<p style=\"margin-bottom:0.75em\">User guide for v0.5 docs.google.com/document/d/1pEobUknMFMkn8F5TMsv8qRzamXX_75BShMMXV8IFslI/edit</p>")
+        i7 = gr.HTML("<p style=\"margin-bottom:0.75em\">Math keyframing explanation docs.google.com/document/d/1pfW1PwbDIuW0cv-dnuyYj1UzPqe23BlSLTJsqazffXM/edit?usp=sharing</p>")
         
         
-        gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">Import settings from file</p>")
+        i8 = gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">Import settings from file</p>")
         with gr.Row():
             override_settings_with_file = gr.Checkbox(label="Override settings", value=False, interactive=True)
             custom_settings_file = gr.Textbox(label="Custom settings file", lines=1, interactive=True)
@@ -32,15 +32,15 @@ class Script(scripts.Script):
             
         # Animation settings START
         #TODO make a some sort of the original dictionary parsing
-        gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">Animation settings</p>")
+        i9 = gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">Animation settings</p>")
         with gr.Row():
             animation_mode = gr.Dropdown(label="animation_mode", choices=['None', '2D', '3D', 'Video Input', 'Interpolation'], value=da.animation_mode, type="index", elem_id="animation_mode", interactive=True)
             max_frames = gr.Number(label="max_frames", value=da.max_frames, interactive=True)
             border = gr.Dropdown(label="border", choices=['replicate', 'wrap'], value=da.border, type="index", elem_id="border", interactive=True)
         
         
-        gr.HTML("<p style=\"margin-bottom:0.75em\">Motion parameters:</p>")
-        gr.HTML("<p style=\"margin-bottom:0.75em\">2D and 3D settings</p>")
+        i10 = gr.HTML("<p style=\"margin-bottom:0.75em\">Motion parameters:</p>")
+        i11 = gr.HTML("<p style=\"margin-bottom:0.75em\">2D and 3D settings</p>")
         with gr.Row():
             angle = gr.Textbox(label="angle", lines=1, value = da.angle, interactive=True)
         with gr.Row():
@@ -58,7 +58,7 @@ class Script(scripts.Script):
             rotation_3d_y = gr.Textbox(label="rotation_3d_y", lines=1, value = da.rotation_3d_y, interactive=True)
         with gr.Row():
             rotation_3d_z = gr.Textbox(label="rotation_3d_z", lines=1, value = da.rotation_3d_z, interactive=True)
-        gr.HTML("<p style=\"margin-bottom:0.75em\">Prespective flip — Low VRAM pseudo-3D mode:</p>")
+        i12 = gr.HTML("<p style=\"margin-bottom:0.75em\">Prespective flip — Low VRAM pseudo-3D mode:</p>")
         with gr.Row():
             flip_2d_perspective = gr.Checkbox(label="flip_2d_perspective", value=da.flip_2d_perspective, interactive=True)
         with gr.Row():
@@ -82,12 +82,12 @@ class Script(scripts.Script):
 #        with gr.Row():
 #            scale_schedule = gr.Textbox(label="scale_schedule", lines=1, value = da.scale_schedule, interactive=True)
         
-        gr.HTML("<p style=\"margin-bottom:0.75em\">Coherence:</p>")
+        i13 = gr.HTML("<p style=\"margin-bottom:0.75em\">Coherence:</p>")
         with gr.Row():
             color_coherence = gr.Dropdown(label="color_coherence", choices=['None', 'Match Frame 0 HSV', 'Match Frame 0 LAB', 'Match Frame 0 RGB'], value=da.color_coherence, type="index", elem_id="color_coherence", interactive=True)
             diffusion_cadence = gr.Slider(label="diffusion_cadence", minimum=1, maximum=8, step=1, value=1, interactive=True)
             
-        gr.HTML("<p style=\"margin-bottom:0.75em\">3D Depth Warping:</p>")
+        i14 = gr.HTML("<p style=\"margin-bottom:0.75em\">3D Depth Warping:</p>")
         with gr.Row():
             use_depth_warping = gr.Checkbox(label="use_depth_warping", value=False, interactive=True)
         with gr.Row():
@@ -99,7 +99,7 @@ class Script(scripts.Script):
             sampling_mode = gr.Dropdown(label="sampling_mode", choices=['bicubic', 'bilinear', 'nearest'], value=da.sampling_mode, type="index", elem_id="sampling_mode", interactive=True)
             save_depth_maps = gr.Checkbox(label="save_depth_maps", value=da.save_depth_maps, interactive=True)
         
-        gr.HTML("<p style=\"margin-bottom:0.75em\">Video Input:</p>")
+        i15 = gr.HTML("<p style=\"margin-bottom:0.75em\">Video Input:</p>")
         with gr.Row():
             video_init_path = gr.Textbox(label="video_init_path", lines=1, value = da.video_init_path, interactive=True)
         with gr.Row():
@@ -109,12 +109,12 @@ class Script(scripts.Script):
         with gr.Row():
             video_mask_path = gr.Textbox(label="video_mask_path", lines=1, value = da.video_mask_path, interactive=True)
         
-        gr.HTML("<p style=\"margin-bottom:0.75em\">Interpolation:</p>")
+        i16 = gr.HTML("<p style=\"margin-bottom:0.75em\">Interpolation:</p>")
         with gr.Row():
             interpolate_key_frames = gr.Checkbox(label="interpolate_key_frames", value=da.interpolate_key_frames, interactive=True)
             interpolate_x_frames = gr.Number(label="interpolate_x_frames", value=da.interpolate_x_frames, interactive=True)
         
-        gr.HTML("<p style=\"margin-bottom:0.75em\">Resume animation:</p>")
+        i17 = gr.HTML("<p style=\"margin-bottom:0.75em\">Resume animation:</p>")
         with gr.Row():
             resume_from_timestring = gr.Checkbox(label="resume_from_timestring", value=da.resume_from_timestring, interactive=True)
             resume_timestring = gr.Textbox(label="resume_timestring", lines=1, value = da.resume_timestring, interactive=True)
@@ -122,13 +122,13 @@ class Script(scripts.Script):
         
         # Prompts settings START
         
-        gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">Prompts</p>")
-        gr.HTML("<p style=\"margin-bottom:0.75em\">`animation_mode: None` batches on list of *prompts*.</p>")
-        gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">*Important change!*</p>")
-        gr.HTML("<p style=\"font-weight:italic;margin-bottom:0.75em\">This script uses the built-in webui weighting settings.</p>")
-        gr.HTML("<p style=\"font-weight:italic;margin-bottom:0.75em\">So if you want to use math functions as prompt weights,</p>")
-        gr.HTML("<p style=\"font-weight:italic;margin-bottom:0.75em\">keep the values above zero in both parts</p>")
-        gr.HTML("<p style=\"font-weight:italic;margin-bottom:0.75em\">Negative prompt part can be specified with --negative</p>")
+        i18 = gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">Prompts</p>")
+        i19 = gr.HTML("<p style=\"margin-bottom:0.75em\">`animation_mode: None` batches on list of *prompts*.</p>")
+        i20 = gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">*Important change from vanilla Deforum!*</p>")
+        i21 = gr.HTML("<p style=\"font-weight:italic;margin-bottom:0.75em\">This script uses the built-in webui weighting settings.</p>")
+        i22 = gr.HTML("<p style=\"font-weight:italic;margin-bottom:0.75em\">So if you want to use math functions as prompt weights,</p>")
+        i23 = gr.HTML("<p style=\"font-weight:italic;margin-bottom:0.75em\">keep the values above zero in both parts</p>")
+        i24 = gr.HTML("<p style=\"font-weight:italic;margin-bottom:0.75em\">Negative prompt part can be specified with --negative</p>")
         with gr.Row():
             prompts = gr.Textbox(label="prompts", lines=8, interactive=True, value = deforum_args.prompts)
         with gr.Row():
@@ -136,20 +136,20 @@ class Script(scripts.Script):
         
         # Prompts settings END
         
-        gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">Run settings</p>")
+        i25 = gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">Run settings</p>")
         
         # Sampling settings START
-        gr.HTML("<p style=\"margin-bottom:0.75em\">Sampling settings</p>")
-        gr.HTML("<p style=\"margin-bottom:0.75em\">The following settings have already been set up in the webui</p>")
-        gr.HTML("<p style=\"margin-bottom:0.75em\">Do you want to override them?</p>")
-        gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">WIP *Doesn't do anything at the moment!*</p>") #TODO
+        i26 = gr.HTML("<p style=\"margin-bottom:0.75em\">Sampling settings</p>")
+        i27 = gr.HTML("<p style=\"margin-bottom:0.75em\">The following settings have already been set up in the webui</p>")
+        i28 = gr.HTML("<p style=\"margin-bottom:0.75em\">Do you want to override them?</p>")
+        i29 = gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">WIP *Doesn't do anything at the moment!*</p>") #TODO
         with gr.Row():
             override_webui_with_these = gr.Checkbox(label="override_webui_with_these", value=False, interactive=True)
-        gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">W, H, seed, sampler, steps, scale, ddim_eta, n_batch, make_grid, grid_rows</p>")
+        i30 = gr.HTML("<p style=\"font-weight:bold;margin-bottom:0.75em\">W, H, seed, sampler, steps, scale, ddim_eta, n_batch, make_grid, grid_rows</p>")
         # Sampling settings END
         
         # Batch settings START
-        gr.HTML("<p style=\"margin-bottom:0.75em\">Batch settings</p>")
+        i31 = gr.HTML("<p style=\"margin-bottom:0.75em\">Batch settings</p>")
         with gr.Row():
             batch_name = gr.Textbox(label="batch_name", lines=1, interactive=True, value = d.batch_name)
         with gr.Row():    
@@ -160,7 +160,7 @@ class Script(scripts.Script):
         # Batch settings END
         
         # Init settings START
-        gr.HTML("<p style=\"margin-bottom:0.75em\">Init settings</p>")
+        i32 = gr.HTML("<p style=\"margin-bottom:0.75em\">Init settings</p>")
         with gr.Row():
             use_init = gr.Checkbox(label="use_init", value=is_img2img, interactive=True, visible=True)
             from_img2img_instead_of_link = gr.Checkbox(label="from_img2img_instead_of_link", value=is_img2img, interactive=True, visible=is_img2img)
@@ -181,10 +181,10 @@ class Script(scripts.Script):
             mask_overlay_blur = gr.Number(label="mask_overlay_blur", value=d.mask_overlay_blur, interactive=True)
         # Init settings END
         
-        return [override_settings_with_file, custom_settings_file, animation_mode, max_frames, border, angle, zoom, translation_x, translation_y, translation_z, rotation_3d_x, rotation_3d_y, rotation_3d_z, flip_2d_perspective, perspective_flip_theta, perspective_flip_phi, perspective_flip_gamma, perspective_flip_fv, noise_schedule, strength_schedule, contrast_schedule, color_coherence, diffusion_cadence, use_depth_warping, midas_weight, near_plane, far_plane, fov, padding_mode, sampling_mode, save_depth_maps, video_init_path, extract_nth_frame, overwrite_extracted_frames, use_mask_video, video_mask_path, interpolate_key_frames, interpolate_x_frames, resume_from_timestring, resume_timestring, prompts, animation_prompts, override_webui_with_these, batch_name, filename_format, seed_behavior, use_init, from_img2img_instead_of_link, strength_0_no_init, strength, init_image, use_mask, use_alpha_as_mask, invert_mask, overlay_mask, mask_file, mask_brightness_adjust, mask_overlay_blur]
+        return [override_settings_with_file, custom_settings_file, animation_mode, max_frames, border, angle, zoom, translation_x, translation_y, translation_z, rotation_3d_x, rotation_3d_y, rotation_3d_z, flip_2d_perspective, perspective_flip_theta, perspective_flip_phi, perspective_flip_gamma, perspective_flip_fv, noise_schedule, strength_schedule, contrast_schedule, color_coherence, diffusion_cadence, use_depth_warping, midas_weight, near_plane, far_plane, fov, padding_mode, sampling_mode, save_depth_maps, video_init_path, extract_nth_frame, overwrite_extracted_frames, use_mask_video, video_mask_path, interpolate_key_frames, interpolate_x_frames, resume_from_timestring, resume_timestring, prompts, animation_prompts, override_webui_with_these, batch_name, filename_format, seed_behavior, use_init, from_img2img_instead_of_link, strength_0_no_init, strength, init_image, use_mask, use_alpha_as_mask, invert_mask, overlay_mask, mask_file, mask_brightness_adjust, mask_overlay_blur, i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14, i15, i16, i17, i18, i19, i20, i21, i22, i23, i24, i25, i26, i27, i28, i29, i30, i31, i32]
 
 
-    def run(self, p, override_settings_with_file, custom_settings_file, animation_mode, max_frames, border, angle, zoom, translation_x, translation_y, translation_z, rotation_3d_x, rotation_3d_y, rotation_3d_z, flip_2d_perspective, perspective_flip_theta, perspective_flip_phi, perspective_flip_gamma, perspective_flip_fv, noise_schedule, strength_schedule, contrast_schedule, color_coherence, diffusion_cadence, use_depth_warping, midas_weight, near_plane, far_plane, fov, padding_mode, sampling_mode, save_depth_maps, video_init_path, extract_nth_frame, overwrite_extracted_frames, use_mask_video, video_mask_path, interpolate_key_frames, interpolate_x_frames, resume_from_timestring, resume_timestring, prompts, animation_prompts, override_webui_with_these, batch_name, filename_format, seed_behavior, use_init, from_img2img_instead_of_link, strength_0_no_init, strength, init_image, use_mask, use_alpha_as_mask, invert_mask, overlay_mask, mask_file, mask_brightness_adjust, mask_overlay_blur):
+    def run(self, p, override_settings_with_file, custom_settings_file, animation_mode, max_frames, border, angle, zoom, translation_x, translation_y, translation_z, rotation_3d_x, rotation_3d_y, rotation_3d_z, flip_2d_perspective, perspective_flip_theta, perspective_flip_phi, perspective_flip_gamma, perspective_flip_fv, noise_schedule, strength_schedule, contrast_schedule, color_coherence, diffusion_cadence, use_depth_warping, midas_weight, near_plane, far_plane, fov, padding_mode, sampling_mode, save_depth_maps, video_init_path, extract_nth_frame, overwrite_extracted_frames, use_mask_video, video_mask_path, interpolate_key_frames, interpolate_x_frames, resume_from_timestring, resume_timestring, prompts, animation_prompts, override_webui_with_these, batch_name, filename_format, seed_behavior, use_init, from_img2img_instead_of_link, strength_0_no_init, strength, init_image, use_mask, use_alpha_as_mask, invert_mask, overlay_mask, mask_file, mask_brightness_adjust, mask_overlay_blur, i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14, i15, i16, i17, i18, i19, i20, i21, i22, i23, i24, i25, i26, i27, i28, i29, i30, i31, i32):
         print('Hello, deforum!')
         
         outdir =  os.path.join(p.outpath_samples, batch_name)
