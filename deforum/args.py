d71dc019890217a91a9fefee0ce2eadb2c41b4f7
kabachuha <artemkhrapov2001@yandex.ru>
2022-10-15 00:05:40 +0300

    make example stuff more painterly

--- deforum/args.py
+++ deforum/args.py
@@ -84,7 +84,7 @@ def DeforumPrompts():
 
 def DeforumAnimPrompts():
     return r"""{
-    "0": "apple:`where(cos(6.28*t/10)>=0, 2*cos(6.28*t/10), 0)`, strawberry:`where(cos(6.28*t/10)<0, -2*cos(6.28*t/10), 0)`, snow --neg apple:`where(cos(6.28*t/10)<0, -2*cos(6.28*t/10), 0)`, strawberry:`where(cos(6.28*t/10)>=0, 2*cos(6.28*t/10), 0)`",
+    "0": "apple:`where(cos(6.28*t/10)>=0, 2*cos(6.28*t/10), 0)`, strawberry:`where(cos(6.28*t/10)<0, -2*cos(6.28*t/10), 0)`, snow, detailed painting by greg rutkowski --neg apple:`where(cos(6.28*t/10)<0, -2*cos(6.28*t/10), 0)`, strawberry:`where(cos(6.28*t/10)>=0, 2*cos(6.28*t/10), 0)`",
     "60": "a beautiful (((banana))), trending on Artstation",
     "80": "a beautiful coconut, trending on Artstation",
     "100": "a beautiful durian, trending on Artstation"
