ca2b7378128d4ca7950fb12b6e6ee1c7b8637418
Funofabot <117096230+Funofabot@users.noreply.github.com>
2022-12-08 05:05:29 -0700

    updated for image loading and masking noise
    changed add_noise to accommodate masking frame noise
    moved image and mask loading functionality to load_images.py
    supplies the mask in args for use in the render_animation step.

--- scripts/deforum_helpers/generate.py
+++ scripts/deforum_helpers/generate.py
@@ -1,5 +1,5 @@
 import torch
-import PIL #HOTFIXISSUE#33 needed for instruction to generate negative mask. 
+import PIL
 from PIL import Image, ImageOps
 import requests
 import numpy as np
@@ -14,6 +14,7 @@ from contextlib import nullcontext
 from einops import rearrange
 
 from .prompt import get_uc_and_c, parse_weight
+from .load_images import load_img, prepare_mask, check_mask_for_errors
 from .k_samplers import sampler_fn
 from scipy.ndimage import gaussian_filter
 
@@ -26,74 +27,17 @@ from modules import processing
 from modules.shared import opts, sd_model
 from modules.processing import process_images, StableDiffusionProcessingTxt2Img
 
-#MASKARGSEXPANSION 
-#Add option to remove noise in relation to masking so that areas which are masked receive less noise
-def add_noise(sample: torch.Tensor, noise_amt: float) -> torch.Tensor:
+def add_noise(sample: torch.Tensor, noise_amt: float, noise_mask = None, invert_mask = False) -> torch.Tensor:
+    if noise_mask is not None:
+        if invert_mask:
+            noise_mask = ImageOps.invert(noise_mask)
+        noise_mask = np.array(noise_mask.convert("L"))
+        noise_mask = noise_mask.astype(np.float32) / 255.0
+        noise_mask = torch.from_numpy(noise_mask)
+        noise_mask = torch.round(noise_mask)
+        return sample + ((torch.randn(sample.shape, device=sample.device) * noise_mask) * noise_amt) 
     return sample + torch.randn(sample.shape, device=sample.device) * noise_amt
-
-def load_img(path, shape, use_alpha_as_mask=False):
-    # use_alpha_as_mask: Read the alpha channel of the image as the mask image
-    if path.startswith('http://') or path.startswith('https://'):
-        image = Image.open(requests.get(path, stream=True).raw)
-    else:
-        image = Image.open(path)
-
-    if use_alpha_as_mask:
-        image = image.convert('RGBA')
-    else:
-        image = image.convert('RGB')
-
-    image = image.resize(shape, resample=Image.LANCZOS)
-
-    mask_image = None
-    if use_alpha_as_mask:
-        # Split alpha channel into a mask_image
-        red, green, blue, alpha = Image.Image.split(image)
-        mask_image = alpha.convert('L')
-        image = image.convert('RGB')
-
-    return image, mask_image #PIL image for auto's pipeline
-
-def load_mask_latent(mask_input, shape):
-    # mask_input (str or PIL Image.Image): Path to the mask image or a PIL Image object
-    # shape (list-like len(4)): shape of the image to match, usually latent_image.shape
-    
-    if isinstance(mask_input, str): # mask input is probably a file name
-        if mask_input.startswith('http://') or mask_input.startswith('https://'):
-            mask_image = Image.open(requests.get(mask_input, stream=True).raw).convert('RGBA')
-        else:
-            mask_image = Image.open(mask_input).convert('RGBA')
-    elif isinstance(mask_input, Image.Image):
-        mask_image = mask_input
-    else:
-        raise Exception("mask_input must be a PIL image or a file name")
-
-    mask_w_h = (shape[-1], shape[-2])
-    mask = mask_image.resize(mask_w_h, resample=Image.LANCZOS)
-    mask = mask.convert("L")
-    return mask
-
-def prepare_mask(mask_input, mask_shape, mask_brightness_adjust=1.0, mask_contrast_adjust=1.0, invert_mask=False):
-    # mask_input (str or PIL Image.Image): Path to the mask image or a PIL Image object
-    # shape (list-like len(4)): shape of the image to match, usually latent_image.shape
-    # mask_brightness_adjust (non-negative float): amount to adjust brightness of the iamge, 
-    #     0 is black, 1 is no adjustment, >1 is brighter
-    # mask_contrast_adjust (non-negative float): amount to adjust contrast of the image, 
-    #     0 is a flat grey image, 1 is no adjustment, >1 is more contrast
-    
-    mask = load_mask_latent(mask_input, mask_shape)
-
-    # Mask brightness/contrast adjustments
-    if mask_brightness_adjust != 1:
-        mask = TF.adjust_brightness(mask, mask_brightness_adjust)
-    if mask_contrast_adjust != 1:
-        mask = TF.adjust_contrast(mask, mask_contrast_adjust)
-
-    if invert_mask:
-        mask = PIL.ImageOps.invert(mask)
-    
-    return mask
-    
+   
 def generate(args, anim_args, root, frame = 0, return_sample=False):
     import re
     assert args.prompt is not None
@@ -149,9 +93,9 @@ def generate(args, anim_args, root, frame = 0, return_sample=False):
         print("\nNo init image, but strength > 0. Strength has been auto set to 0, since use_init is False.")
         print("If you want to force strength > 0 with no init, please set strength_0_no_init to False.\n")
         args.strength = 0
+   
     mask_image = None
     init_image = None
-    
     processed = None
     
     if args.init_sample is not None:
@@ -191,25 +135,26 @@ def generate(args, anim_args, root, frame = 0, return_sample=False):
             )
         processed = processing.process_images(p_txt)
     
-    if processed is None:
+   if processed is None:
         # Mask functions
         if args.use_mask:
             assert args.mask_file is not None or mask_image is not None, "use_mask==True: An mask image is required for a mask. Please enter a mask_file or use an init image with an alpha channel"
             assert args.use_init, "use_mask==True: use_init is required for a mask"
             mask = prepare_mask(args.mask_file if mask_image is None else mask_image, 
-                                (args.W, args.H), 
+                                (args.W, args.H),
                                 args.mask_contrast_adjust, 
-                                args.mask_brightness_adjust, 
-                                args.invert_mask)
-                                
-            p.inpainting_fill = args.fill # need to come up with better name. 
-            p.inpaint_full_res= args.full_res_mask 
-            p.inpaint_full_res_padding = args.full_res_mask_padding 
+                                args.mask_brightness_adjust)
+            #prevent loaded mask from throwing errors in Image operations if completely black and crop and resize in webui pipeline
+            #doing this after contrast and brightness adjustments to ensure that mask is not passed as black or blank
+            mask = check_mask_for_errors(mask, args.invert_mask)
 
-            #if (torch.all(mask == 0) or torch.all(mask == 1)) and args.use_alpha_as_mask:
-            #    raise Warning("use_alpha_as_mask==True: Using the alpha channel from the init image as a mask, but the alpha channel is blank.")
-            
-            #mask = repeat(mask, '1 ... -> b ...', b=batch_size)
+            #assign masking options to pipeline
+            if mask is not None:
+                p.inpainting_mask_invert = args.invert_mask
+                p.inpainting_fill = args.fill 
+                p.inpaint_full_res= args.full_res_mask 
+                p.inpaint_full_res_padding = args.full_res_mask_padding 
+
         else:
             mask = None
 
@@ -217,6 +162,7 @@ def generate(args, anim_args, root, frame = 0, return_sample=False):
         
         p.init_images = [init_image]
         p.image_mask = mask
+        args.mask_image = mask
 
         processed = processing.process_images(p)
     
