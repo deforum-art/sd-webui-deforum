e22d6edc575fa92a32792840598357b997a2ca2b
kishan <phi.liney@gmail.com>
2022-12-18 13:21:44 -0800

    Always load the midas model on predict_depths
    Before, the midas model would only be loaded if the midas_weight
    was 1.0 or higher. With this change, we sync the logic between the
    main deforum-stable-diffusion v.6 repo and this one.

--- scripts/deforum_helpers/render.py
+++ scripts/deforum_helpers/render.py
@@ -81,9 +81,8 @@ def render_animation(args, anim_args, parseq_args, animation_prompts, root):
     predict_depths = (anim_args.animation_mode == '3D' and anim_args.use_depth_warping) or anim_args.save_depth_maps
     if predict_depths:
         depth_model = DepthModel(root.device)
-        if anim_args.midas_weight >= 1.0:
-            depth_model.load_midas(root.models_path, root.half_precision)
-        else:
+        depth_model.load_midas(root.models_path, root.half_precision)
+        if anim_args.midas_weight < 1.0:
             depth_model.load_adabins(root.models_path)
     else:
         depth_model = None
