cc52c96a61509577518c7cace90dd9f2db8b3f66
reallybigname <reallybigname@gmail.com>
2023-01-04 06:35:51 -0800

    Hybrid Video Compositing - 3D working 2D broken
    First commit. 3D mode works

--- scripts/deforum_helpers/seed.py
+++ scripts/deforum_helpers/seed.py
@@ -2,7 +2,23 @@ import random
 
 def next_seed(args):
     if args.seed_behavior == 'iter':
-        args.seed += 1
+        if args.seed_internal % args.seed_iter_N == 0:
+            args.seed += 1
+        args.seed_internal += 1
+    elif args.seed_behavior == 'ladder':
+        if args.seed_internal == 0:
+            args.seed += 2
+            args.seed_internal = 1
+        else:
+            args.seed -= 1
+            args.seed_internal = 0
+    elif args.seed_behavior == 'alternate':
+        if args.seed_internal == 0:
+            args.seed += 1
+            args.seed_internal = 1
+        else:
+            args.seed -= 1
+            args.seed_internal = 0
     elif args.seed_behavior == 'fixed':
         pass # always keep seed the same
     else:
