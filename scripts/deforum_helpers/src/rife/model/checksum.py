8ff265f7db825e2c7daedafad543dba9a39ab254
hithereai <121192995+hithereai@users.noreply.github.com>
2023-01-20 20:27:52 +0200

    remove print statement

--- scripts/deforum_helpers/src/rife/model/checksum.py
+++ scripts/deforum_helpers/src/rife/model/checksum.py
@@ -7,5 +7,3 @@ def checksum(filename, hash_factory=hashlib.blake2b, chunk_num_blocks=128):
         while chunk := f.read(chunk_num_blocks*h.block_size): 
             h.update(chunk)
     return h.hexdigest()
-
-#print(checksum('TEST_PATH'))
