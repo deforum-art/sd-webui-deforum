f1a093e0ece2a1ac99e39708aaa4334dee0c08f3
hithereai <121192995+hithereai@users.noreply.github.com>
2023-01-27 02:00:14 +0200

    add dynamic ffmpeg_location to frame_interpolation.py

--- scripts/deforum_helpers/frame_interpolation.py
+++ scripts/deforum_helpers/frame_interpolation.py
@@ -9,7 +9,7 @@ def extract_rife_name(string):
         raise ValueError("Input string should contain exactly 2 words, first word should be 'RIFE' and second word should start with 'v' followed by 2 numbers")
     return "RIFE"+parts[1][1:].replace('.','')
    
-def process_video_interpolation(frame_interpolation_engine=None, frame_interpolation_x_amount="Disabled", frame_interpolation_slow_mo_amount="Disabled", orig_vid_fps=None, deforum_models_path=None, real_audio_track=None, raw_output_imgs_path=None, img_batch_id=None, ffmpeg_crf=None, ffmpeg_preset=None, keep_interp_imgs=False):
+def process_video_interpolation(frame_interpolation_engine=None, frame_interpolation_x_amount="Disabled", frame_interpolation_slow_mo_amount="Disabled", orig_vid_fps=None, deforum_models_path=None, real_audio_track=None, raw_output_imgs_path=None, img_batch_id=None, ffmpeg_location=None, ffmpeg_crf=None, ffmpeg_preset=None, keep_interp_imgs=False):
 
     if frame_interpolation_x_amount != "Disabled":
         
@@ -36,5 +36,5 @@ def process_video_interpolation(frame_interpolation_engine=None, frame_interpola
                 
             # run actual interpolation and video stitching etc - the whole suite
             if actual_model_folder_name:
-                run_rife_new_video_infer(interp_x_amount=interp_amount_clean_num, slow_mo_x_amount=interp_slow_mo_clean_num, output=None, model=actual_model_folder_name, fps=fps, deforum_models_path=deforum_models_path, audio_track=real_audio_track, raw_output_imgs_path=raw_output_imgs_path, img_batch_id=img_batch_id, ffmpeg_crf=ffmpeg_crf, ffmpeg_preset=ffmpeg_preset, keep_imgs=keep_interp_imgs)
+                run_rife_new_video_infer(interp_x_amount=interp_amount_clean_num, slow_mo_x_amount=interp_slow_mo_clean_num, output=None, model=actual_model_folder_name, fps=fps, deforum_models_path=deforum_models_path, audio_track=real_audio_track, raw_output_imgs_path=raw_output_imgs_path, img_batch_id=img_batch_id, ffmpeg_location=ffmpeg_location, ffmpeg_crf=ffmpeg_crf, ffmpeg_preset=ffmpeg_preset, keep_imgs=keep_interp_imgs)
              
