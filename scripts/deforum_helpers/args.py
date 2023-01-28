61847887259b55712f80c57944b3bfb760b7fdfe
hithereai <121192995+hithereai@users.noreply.github.com>
2023-01-28 14:03:32 +0200

    rename frame interpolation text

--- scripts/deforum_helpers/args.py
+++ scripts/deforum_helpers/args.py
@@ -645,7 +645,7 @@ def setup_deforum_setting_dictionary(self, is_img2img, is_extension = True):
                 mp4_path = gr.Textbox(label="mp4_path", lines=1, interactive=True, value = dv.mp4_path)
             with gr.Row():
                 store_frames_in_ram = gr.Checkbox(label="store_frames_in_ram", value=dv.store_frames_in_ram, interactive=True)
-        with gr.Accordion('Frame Interpolation', open=False):
+        with gr.Accordion('Frame Interpolation (RIFE)', open=False):
             gr.HTML("""
             Use RIFE and other Video Frame Interpolation methods to smooth out, slow-mo (or both) your output videos.</p>
              <p style="margin-top:1em">
