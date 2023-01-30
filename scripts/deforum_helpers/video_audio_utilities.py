a31ae68133410fc39e1b5caa319ec84f22939ca1
hithereai <121192995+hithereai@users.noreply.github.com>
2023-01-30 19:09:26 +0200

    fixes

--- scripts/deforum_helpers/video_audio_utilities.py
+++ scripts/deforum_helpers/video_audio_utilities.py
@@ -11,7 +11,9 @@ def get_frame_name(path):
     name = os.path.splitext(name)[0]
     return name
 
-def is_vid_path_valid(video_path) -> bool:
+# this functions makes sure the video_path provided is an existing local file or a web URL
+# todo: check file extensions!
+def is_vid_path_valid(video_path):
     if video_path.startswith('http://') or video_path.startswith('https://'):
         response = requests.head(video_path)
         if response.status_code == 404 or response.status_code != 200:
@@ -23,18 +25,19 @@ def is_vid_path_valid(video_path) -> bool:
             raise RuntimeError("Init video path or mask video path is not valid")
     return True
     
+
 def vid2frames(video_path, video_in_frame_path, n=1, overwrite=True, extract_from_frame=0, extract_to_frame=-1, only_get_fps=False): 
-    # TODO: add optional out/ in file format (png or jpg?)
-    img_format = 'png'
+    #todo? get the name of the video without the path and ext
     
     if (extract_to_frame <= extract_from_frame) and extract_to_frame != -1:
         raise RuntimeError('extract_to_frame can not be highher than extract_from_frame')
-    name = get_frame_name(video_path)
+    
     if n < 1: n = 1 #HACK Gradio interface does not currently allow min/max in gr.Number(...) 
 
-    # make sure vid is a valid active url or an existing local folder
     if is_vid_path_valid(video_path):
-               
+        
+        name = get_frame_name(video_path)
+        
         vidcap = cv2.VideoCapture(video_path)
         video_fps = vidcap.get(cv2.CAP_PROP_FPS)
 
@@ -52,8 +55,6 @@ def vid2frames(video_path, video_in_frame_path, n=1, overwrite=True, extract_fro
 
             # grab the frame count to check against existing directory len 
             frame_count = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT)) 
-            
-            print(f"Extracting {frame_count} frames from video... Please wait.")
             
             # raise error if the user wants to skip more frames than exist
             if n >= frame_count : 
@@ -76,11 +77,11 @@ def vid2frames(video_path, video_in_frame_path, n=1, overwrite=True, extract_fro
                     if state.interrupted:
                         return
                     if (count <= extract_to_frame or extract_to_frame == -1) and count % n == 0:
-                        cv2.imwrite(video_in_frame_path + os.path.sep + name + f"{t:05}." + img_format , image) # save frame as JPEG file
+                        cv2.imwrite(video_in_frame_path + os.path.sep + name + f"{t:05}.jpg" , image)     # save frame as JPEG file
                         t += 1
                     success,image = vidcap.read()
                     count += 1
-                print("Extracted %d frames" % count)
+                print("Converted %d frames" % count)
             else:
                 print("Frames already unpacked")
         vidcap.release()
@@ -117,7 +118,7 @@ def ffmpegvid2frames(full_vid_path = None, full_out_imgs_path = None, out_img_fo
     except Exception as e:
         raise Exception(f'Error extracting frames from video. Actual runtime error:{e}.')
     extracted_frame_count = count_files(full_out_imgs_path)
-    print(f"Extracted {extracted_frame_count} out of {vid_to_interp_input_frame_count} frames from video:\n{full_vid_path}\nTo folder:\n{full_out_imgs_path}")
+    print(f"Extracted {extracted_frame_count} (out of {vid_to_interp_input_frame_count}) frames from video:\n{full_vid_path}\nTo folder:\n{full_out_imgs_path}")
     return extracted_frame_count
 
 def get_next_frame(outdir, video_path, frame_idx, mask=False):
