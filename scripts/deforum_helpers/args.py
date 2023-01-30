632fc37056eb3467942a99983892a4f1af3ef21a
hithereai <121192995+hithereai@users.noreply.github.com>
2023-01-30 12:27:55 +0200

    get input vid to interp fps

--- scripts/deforum_helpers/args.py
+++ scripts/deforum_helpers/args.py
@@ -4,6 +4,7 @@ import modules.shared as sh
 import modules.paths as ph
 import os
 from pkg_resources import resource_filename
+from .vid2frames import vid2frames
 
 def Root():
     device = sh.device
@@ -756,6 +757,10 @@ def setup_deforum_setting_dictionary(self, is_img2img, is_extension = True):
                                     print("Please set a proper value for 'Interp x'. Can't interpolate x0 times :)")
                                 else:
                                     print(f"** Got a request to frame-interpolate a video! **\nVid to interpolate: {file.orig_name}\nInteroplating using {engine}, {x_am} times with slow-mo set to {sl_am}.")
+                                    outdir = 'D:/D-SD/autopt2NEW/stable-diffusion-webui/outputs/img2img-images/Deforum'
+                                    vid_to_interp_pngs_tmp_folder = video_in_frame_path = os.path.join(outdir, 'TEST_INTERP')
+                                    vid_to_interp_fps = vid2frames(file.name, video_in_frame_path, 1, True, 0, -1, True)
+                                    print(f"input vid fps: {vid_to_interp_fps}")
                             else:
                                 print("Found no uploaded video to interpolate on. Make sure the upload box is showing the video you tried to upload.")
                         # handle video to frames with vid2frames or ffmpeg - need to check implemn
