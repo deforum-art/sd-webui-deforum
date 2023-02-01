7660309bf52215084d44289d96688d97626ab2e3
hithereai <121192995+hithereai@users.noreply.github.com>
2023-02-01 21:01:10 +0200

    better ffmpeg not found msgs

--- scripts/deforum_helpers/video_audio_utilities.py
+++ scripts/deforum_helpers/video_audio_utilities.py
@@ -103,48 +103,54 @@ def get_vid_fps_and_frame_count(vid_local_path):
 def ffmpeg_stitch_video(ffmpeg_location=None, fps=None, outmp4_path=None, stitch_from_frame=0, stitch_to_frame=None, imgs_path=None, add_soundtrack=None, audio_path=None, crf=17, preset='veryslow'):
     # TODO: add audio custom print msgs for a nice user experience
     print(f"Trying to stitch video from frames using FFMPEG:\nFrames:\n{imgs_path}\nTo Video:\n{outmp4_path}")
-    cmd = [
-        ffmpeg_location,
-        '-y',
-        '-vcodec', 'png',
-        '-r', str(int(fps)),
-        '-start_number', str(stitch_from_frame),
-        '-i', imgs_path,
-        '-frames:v', str(stitch_to_frame),
-        '-c:v', 'libx264',
-        '-vf',
-        f'fps={int(fps)}',
-        '-pix_fmt', 'yuv420p',
-        '-crf', str(crf),
-        '-preset', preset,
-        '-pattern_type', 'sequence',
-        outmp4_path
-    ]
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    if process.returncode != 0:
-        print(stderr)
-        raise RuntimeError(stderr)
-
-    if add_soundtrack != 'None':
+    try:
         cmd = [
             ffmpeg_location,
-            '-i',
-            outmp4_path,
-            '-i',
-            audio_path,
-            '-map', '0:v',
-            '-map', '1:a',
-            '-c:v', 'copy',
-            '-shortest',
-            outmp4_path+'.temp.mp4'
+            '-y',
+            '-vcodec', 'png',
+            '-r', str(int(fps)),
+            '-start_number', str(stitch_from_frame),
+            '-i', imgs_path,
+            '-frames:v', str(stitch_to_frame),
+            '-c:v', 'libx264',
+            '-vf',
+            f'fps={int(fps)}',
+            '-pix_fmt', 'yuv420p',
+            '-crf', str(crf),
+            '-preset', preset,
+            '-pattern_type', 'sequence',
+            outmp4_path
         ]
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
-        if process.returncode != 0:
-            print(stderr)
-            raise RuntimeError(stderr)
-        os.replace(outmp4_path+'.temp.mp4', outmp4_path)
+    except FileNotFoundError:
+        raise FileNotFoundError("FFmpeg not found. Please make sure you have a working ffmpeg path under 'ffmpeg_location' parameter.") # \n*Interpolated frames were SAVED as backup!*")
+    except Exception as e:
+        raise Exception(f'Error stitching frames to video. Actual runtime error:{e}') #\n*Interpolated frames were SAVED as backup!*')
+
+    if add_soundtrack != 'None':
+        try:
+            cmd = [
+                ffmpeg_location,
+                '-i',
+                outmp4_path,
+                '-i',
+                audio_path,
+                '-map', '0:v',
+                '-map', '1:a',
+                '-c:v', 'copy',
+                '-shortest',
+                outmp4_path+'.temp.mp4'
+            ]
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate()
+            if process.returncode != 0:
+                print(stderr)
+                raise RuntimeError(stderr)
+            os.replace(outmp4_path+'.temp.mp4', outmp4_path)
+        except Exception as e:
+            print(f'Error adding audio to video. Actual error: {e}')
+    # If we reached this point, all ok. Let the user know!
     print("FFMPEG Video Stitching done!")
 
 def get_frame_name(path):
