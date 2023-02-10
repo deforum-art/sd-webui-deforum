abff7a6432bfc674540187d561dbb78d9a92a306
hithereai <121192995+hithereai@users.noreply.github.com>
2023-02-11 01:13:37 +0200

    add elem_id to top info accord

--- scripts/deforum_helpers/args.py
+++ scripts/deforum_helpers/args.py
@@ -307,7 +307,7 @@ def setup_deforum_setting_dictionary(self, is_img2img, is_extension = True):
         btn = i1 = gr.HTML("")
         
     # MAIN (TOP) EXTENSION INFO ACCORD
-    with gr.Accordion("Info, Links and Help", open=False):
+    with gr.Accordion("Info, Links and Help", open=False, elem_id='main_top_info_accord'):
             gr.HTML("""<strong>Made by <a href="https://deforum.github.io">deforum.github.io</a>, port for AUTOMATIC1111's webui maintained by <a href="https://github.com/kabachuha">kabachuha</a></strong>""")
             gr.HTML("""<a  style="color:SteelBlue" href="https://github.com/deforum-art/deforum-for-automatic1111-webui/wiki/FAQ-&-Troubleshooting">FOR HELP CLICK HERE</a""", elem_id="for_help_click_here")
             gr.HTML("""<ul style="list-style-type:circle; margin-left:1em">
