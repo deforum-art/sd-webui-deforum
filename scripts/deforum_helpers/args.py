0afa6bf45d35c36aecd72b5df022c213cce72baf
hithereai <121192995+hithereai@users.noreply.github.com>
2023-02-11 00:58:23 +0200

    del old rife msg about gifs

--- scripts/deforum_helpers/args.py
+++ scripts/deforum_helpers/args.py
@@ -776,7 +776,6 @@ def setup_deforum_setting_dictionary(self, is_img2img, is_extension = True):
                             <li>Frame Interpolation will *not* run if 'store_frames_in_ram' is enabled.</li>
                             <li>Audio (if provided) will *not* be transferred to the interpolated video if Slow-Mo is enabled.</li>
                             <li>'add_soundtrack' and 'soundtrack_path' aren't being honoured in "Interpolate an existing video" mode. Original vid audio will be used instead with the same slow-mo rules above.</li>
-                            <li>Frame Interpolation will always save an .mp4 video even if you used GIF for the raw video.</li>
                         </ul>
                     </p>
                     """)
