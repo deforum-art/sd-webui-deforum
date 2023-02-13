7f49195fcb0dd73ed054533ac27bf6d8e3bcc72d
hithereai <121192995+hithereai@users.noreply.github.com>
2023-02-13 03:31:01 +0200

    clean

--- scripts/deforum_helpers/general_utils.py
+++ scripts/deforum_helpers/general_utils.py
@@ -1,8 +1,5 @@
 import hashlib
-
-# TODO: move this to a util file and remove the duplication of it in rife's src model checksum.py
 def checksum(filename, hash_factory=hashlib.blake2b, chunk_num_blocks=128):
-    
     h = hash_factory()
     with open(filename,'rb') as f: 
         while chunk := f.read(chunk_num_blocks*h.block_size): 
