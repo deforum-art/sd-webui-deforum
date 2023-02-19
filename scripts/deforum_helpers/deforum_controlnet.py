3114a3bb689a6b0cd10ccd095fbf6bc55de6d58e
kabachuha <artemkhrapov2001@yandex.ru>
2023-02-19 17:21:43 +0300

    fix gradio rows
    gradio rows don't seem to support dynamic component renamings, so hardcode them

--- scripts/deforum_helpers/deforum_controlnet.py
+++ scripts/deforum_helpers/deforum_controlnet.py
@@ -56,14 +56,16 @@ def setup_controlnet_ui_raw():
     # Video input to be fed into ControlNet
     #input_video_url = gr.Textbox(source='upload', type='numpy', tool='sketch') # TODO
     with gr.Row():
-        input_video_chosen_file = gr.File(label="ControlNet Video input", interactive=True, file_count="single", file_types=["video"], elem_id="controlnet_input_video_chosen_file")
+        controlnet_input_video_chosen_file = gr.File(label="ControlNet Video Input", interactive=True, file_count="single", file_types=["video"], elem_id="controlnet_input_video_chosen_file")
+    with gr.Row():
+        controlnet_input_video_mask_chosen_file = gr.File(label="ControlNet Video Mask Input", interactive=True, file_count="single", file_types=["video"], elem_id="controlnet_input_video_mask_chosen_file")
 
     # Copying the main ControlNet widgets while getting rid of static elements such as the scribble pad
     with gr.Row():
-        enabled = gr.Checkbox(label='Enable', value=False)
-        scribble_mode = gr.Checkbox(label='Scribble Mode (Invert colors)', value=False)
-        rgbbgr_mode = gr.Checkbox(label='RGB to BGR', value=False)
-        lowvram = gr.Checkbox(label='Low VRAM', value=False)
+        controlnet_enabled = gr.Checkbox(label='Enable', value=False)
+        controlnet_scribble_mode = gr.Checkbox(label='Scribble Mode (Invert colors)', value=False)
+        controlnet_rgbbgr_mode = gr.Checkbox(label='RGB to BGR', value=False)
+        controlnet_lowvram = gr.Checkbox(label='Low VRAM', value=False)
 
     # Main part
     class ToolButton(gr.Button, gr.components.FormComponent):
@@ -104,69 +106,69 @@ def setup_controlnet_ui_raw():
     switch_values_symbol = '\U000021C5' # ⇅
 
     with gr.Row():
-        module = gr.Dropdown(list(preprocessor.keys()), label=f"Preprocessor", value="none")
-        model = gr.Dropdown(list(cn_models.keys()), label=f"Model", value="None")
+        controlnet_module = gr.Dropdown(list(preprocessor.keys()), label=f"Preprocessor", value="none")
+        controlnet_model = gr.Dropdown(list(cn_models.keys()), label=f"Model", value="None")
         refresh_models = ToolButton(value=refresh_symbol)
-        refresh_models.click(refresh_all_models, model, model)
+        refresh_models.click(refresh_all_models, controlnet_model, controlnet_model)
         # ctrls += (refresh_models, )
     with gr.Row():
-        weight = gr.Slider(label=f"Weight", value=1.0, minimum=0.0, maximum=2.0, step=.05)
-        guidance_strength =  gr.Slider(label="Guidance strength (T)", value=1.0, minimum=0.0, maximum=1.0, interactive=True)
+        controlnet_weight = gr.Slider(label=f"Weight", value=1.0, minimum=0.0, maximum=2.0, step=.05)
+        controlnet_guidance_strength =  gr.Slider(label="Guidance strength (T)", value=1.0, minimum=0.0, maximum=1.0, interactive=True)
 
         # ctrls += (module, model, weight,)
         # model_dropdowns.append(model)
         
     def build_sliders(module):
-        if module == "canny":
+        if controlnet_module == "canny":
             return [
                 gr.update(label="Annotator resolution", value=512, minimum=64, maximum=2048, step=1, interactive=True),
                 gr.update(label="Canny low threshold", minimum=1, maximum=255, value=100, step=1, interactive=True),
                 gr.update(label="Canny high threshold", minimum=1, maximum=255, value=200, step=1, interactive=True),
                 gr.update(visible=True)
             ]
-        elif module == "mlsd": #Hough
+        elif controlnet_module == "mlsd": #Hough
             return [
                 gr.update(label="Hough Resolution", minimum=64, maximum=2048, value=512, step=1, interactive=True),
                 gr.update(label="Hough value threshold (MLSD)", minimum=0.01, maximum=2.0, value=0.1, step=0.01, interactive=True),
                 gr.update(label="Hough distance threshold (MLSD)", minimum=0.01, maximum=20.0, value=0.1, step=0.01, interactive=True),
                 gr.update(visible=True)
             ]
-        elif module in ["hed", "fake_scribble"]:
+        elif controlnet_module in ["hed", "fake_scribble"]:
             return [
                 gr.update(label="HED Resolution", minimum=64, maximum=2048, value=512, step=1, interactive=True),
                 gr.update(label="Threshold A", value=64, minimum=64, maximum=1024, interactive=False),
                 gr.update(label="Threshold B", value=64, minimum=64, maximum=1024, interactive=False),
                 gr.update(visible=True)
             ]
-        elif module in ["openpose", "openpose_hand", "segmentation"]:
+        elif controlnet_module in ["openpose", "openpose_hand", "segmentation"]:
             return [
                 gr.update(label="Annotator Resolution", minimum=64, maximum=2048, value=512, step=1, interactive=True),
                 gr.update(label="Threshold A", value=64, minimum=64, maximum=1024, interactive=False),
                 gr.update(label="Threshold B", value=64, minimum=64, maximum=1024, interactive=False),
                 gr.update(visible=True)
             ]
-        elif module == "depth":
+        elif controlnet_module == "depth":
             return [
                 gr.update(label="Midas Resolution", minimum=64, maximum=2048, value=384, step=1, interactive=True),
                 gr.update(label="Threshold A", value=64, minimum=64, maximum=1024, interactive=False),
                 gr.update(label="Threshold B", value=64, minimum=64, maximum=1024, interactive=False),
                 gr.update(visible=True)
             ]
-        elif module == "depth_leres":
+        elif controlnet_module == "depth_leres":
             return [
                 gr.update(label="LeReS Resolution", minimum=64, maximum=2048, value=512, step=1, interactive=True),
                 gr.update(label="Remove Near %", value=0, minimum=0, maximum=100, step=0.1, interactive=True),
                 gr.update(label="Remove Background %", value=0, minimum=0, maximum=100, step=0.1, interactive=True),
                 gr.update(visible=True)
             ]
-        elif module == "normal_map":
+        elif controlnet_module == "normal_map":
             return [
                 gr.update(label="Normal Resolution", minimum=64, maximum=2048, value=512, step=1, interactive=True),
                 gr.update(label="Normal background threshold", minimum=0.0, maximum=1.0, value=0.4, step=0.01, interactive=True),
                 gr.update(label="Threshold B", value=64, minimum=64, maximum=1024, interactive=False),
                 gr.update(visible=True)
             ]
-        elif module == "none":
+        elif controlnet_module == "none":
             return [
                 gr.update(label="Normal Resolution", value=64, minimum=64, maximum=2048, interactive=False),
                 gr.update(label="Threshold A", value=64, minimum=64, maximum=1024, interactive=False),
@@ -182,19 +184,19 @@ def setup_controlnet_ui_raw():
             ]
         
     # advanced options    
-    advanced = gr.Column(visible=False)
-    with advanced:
-        processor_res = gr.Slider(label="Annotator resolution", value=64, minimum=64, maximum=2048, interactive=False)
-        threshold_a =  gr.Slider(label="Threshold A", value=64, minimum=64, maximum=1024, interactive=False)
-        threshold_b =  gr.Slider(label="Threshold B", value=64, minimum=64, maximum=1024, interactive=False)
+    controlnet_advanced = gr.Column(visible=False)
+    with controlnet_advanced:
+        controlnet_processor_res = gr.Slider(label="Annotator resolution", value=64, minimum=64, maximum=2048, interactive=False)
+        controlnet_threshold_a =  gr.Slider(label="Threshold A", value=64, minimum=64, maximum=1024, interactive=False)
+        controlnet_threshold_b =  gr.Slider(label="Threshold B", value=64, minimum=64, maximum=1024, interactive=False)
     
     if gradio_compat:    
-        module.change(build_sliders, inputs=[module], outputs=[processor_res, threshold_a, threshold_b, advanced])
+        controlnet_module.change(build_sliders, inputs=[controlnet_module], outputs=[controlnet_processor_res, controlnet_threshold_a, controlnet_threshold_b, controlnet_advanced])
         
     infotext_fields.extend([
-        (module, f"ControlNet Preprocessor"),
-        (model, f"ControlNet Model"),
-        (weight, f"ControlNet Weight"),
+        (controlnet_module, f"ControlNet Preprocessor"),
+        (controlnet_model, f"ControlNet Model"),
+        (controlnet_weight, f"ControlNet Weight"),
     ])
 
     # def svgPreprocess(inputs):
@@ -210,7 +212,7 @@ def setup_controlnet_ui_raw():
     #         return input_image.orgpreprocess(inputs)
     #     return None
 
-    resize_mode = gr.Radio(choices=["Envelope (Outer Fit)", "Scale to Fit (Inner Fit)", "Just Resize"], value="Scale to Fit (Inner Fit)", label="Resize Mode")
+    controlnet_resize_mode = gr.Radio(choices=["Envelope (Outer Fit)", "Scale to Fit (Inner Fit)", "Just Resize"], value="Scale to Fit (Inner Fit)", label="Resize Mode")
 
     return locals()
 
@@ -221,21 +223,19 @@ def setup_controlnet_ui():
                 """)
         return {}
 
-    args_dict = setup_controlnet_ui_raw()
-    ret_dict = {}
-
-    for k, v in args_dict.items():
-        ret_dict["controlnet_" + k] = v
-    
-    return ret_dict
+    return setup_controlnet_ui_raw()
 
 
 def controlnet_component_names():
     if not find_controlnet():
         return []
 
-    controlnet_args_names = str(r'''use_looper, init_images, image_strength_schedule, blendFactorMax, blendFactorSlope, 
-            tweening_frames_schedule, color_correction_factor'''
+    controlnet_args_names = str(r'''controlnet_input_video_chosen_file, controlnet_input_video_mask_chosen_file,
+controlnet_enabled, controlnet_scribble_mode, controlnet_rgbbgr_mode, controlnet_lowvram,
+controlnet_module, controlnet_model,
+controlnet_weight, controlnet_guidance_strength,
+controlnet_processor_res, 
+controlnet_threshold_a, controlnet_threshold_b, controlnet_resize_mode'''
     ).replace("\n", "").replace("\r", "").replace(" ", "").split(',')
     
     return controlnet_args_names
