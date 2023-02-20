f7f85f7b8b688a26ec1983d360e0e30f9b5370e9
hithereai <121192995+hithereai@users.noreply.github.com>
2023-02-20 02:08:22 +0200

    fix ui for mobile/ tiny screens

--- scripts/deforum_helpers/args.py
+++ scripts/deforum_helpers/args.py
@@ -792,15 +792,17 @@ def setup_deforum_setting_dictionary(self, is_img2img, is_extension = True):
                         frame_interpolation_slow_mo_enabled = gr.Checkbox(label="Slow Mo", elem_id="frame_interpolation_slow_mo_enabled", value=dv.frame_interpolation_slow_mo_enabled, interactive=True)
                         # If this is set to True, we keep all of the interpolated frames in a folder. Default is False - means we delete them at the end of the run
                         frame_interpolation_keep_imgs = gr.Checkbox(label="Keep Imgs", elem_id="frame_interpolation_keep_imgs", value=dv.frame_interpolation_keep_imgs, interactive=True)
-                    with gr.Row():
-                        # How many times to interpolate (interp x)
-                        frame_interpolation_x_amount = gr.Slider(minimum=2, maximum=10, step=1, label="Interp x", value=dv.frame_interpolation_x_amount, interactive=True)
-                        # Interp Slow-Mo (setting final output fps, not really doing anything direclty with RIFE)
-                        frame_interpolation_slow_mo_amount =  gr.Slider(minimum=2, maximum=10, step=1, label="Slow-Mo x", value=dv.frame_interpolation_x_amount, interactive=True, visible=False)
+                    with gr.Row(variant='compact'):
+                        with gr.Column(min_width=180) as frame_interp_x_amount_column:
+                            # How many times to interpolate (interp x)
+                            frame_interpolation_x_amount = gr.Slider(minimum=2, maximum=10, step=1, label="Interp x", value=dv.frame_interpolation_x_amount, interactive=True)
+                        with gr.Column(min_width=180, visible=False) as frame_interp_slow_mo_amount_column:
+                            # Interp Slow-Mo (setting final output fps, not really doing anything direclty with RIFE)
+                            frame_interpolation_slow_mo_amount =  gr.Slider(minimum=2, maximum=10, step=1, label="Slow-Mo x", value=dv.frame_interpolation_x_amount, interactive=True)
                     # TODO: move these from here when done
                     def hide_slow_mo(choice):
                         return gr.update(visible=True) if choice else gr.update(visible=False)
-                    frame_interpolation_slow_mo_enabled.change(fn=hide_slow_mo,inputs=frame_interpolation_slow_mo_enabled,outputs=frame_interpolation_slow_mo_amount)
+                    frame_interpolation_slow_mo_enabled.change(fn=hide_slow_mo,inputs=frame_interpolation_slow_mo_enabled,outputs=frame_interp_slow_mo_amount_column)
                     with gr.Row():
                         # Intrpolate any existing video from the connected PC
                         with gr.Accordion('Interpolate an existing video', open=False):
