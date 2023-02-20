01abc3c7369b3d4779a7be6a6a08d2c069ee3aef
kabachuha <artemkhrapov2001@yandex.ru>
2023-02-20 23:33:21 +0300

    use_init note

--- scripts/deforum_helpers/args.py
+++ scripts/deforum_helpers/args.py
@@ -680,7 +680,7 @@ def setup_deforum_setting_dictionary(self, is_img2img, is_extension = True):
                     *Work In Progress*. All params below are going to be keyframable at some point. If you want to speedup the integration, join Deforum's development. &#128521;
                 </p>
                 <p">
-                    Due to ControlNet base extension's inner works it needs its models to be located at 'extensions/deforum-for-automatic1111-webui/models'. So copy, symlink or move them there until a more elegant solution is found.
+                    Due to ControlNet base extension's inner works it needs its models to be located at 'extensions/deforum-for-automatic1111-webui/models'. So copy, symlink or move them there until a more elegant solution is found. And, as of now, it requires use_init checked for the first run.
                 </p>
                 """)
                 controlnet_dict = setup_controlnet_ui()
