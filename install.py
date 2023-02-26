156b8870e60ce941e3c8a1cef959c50b16cb174d
hithereai <121192995+hithereai@users.noreply.github.com>
2023-02-26 02:08:30 +0200

    revert install.py to original state - no colab mess needed

--- install.py
+++ install.py
@@ -1,6 +1,5 @@
 import launch
 import os
-import sys
 
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 
@@ -8,7 +7,4 @@ with open(req_file) as file:
     for lib in file:
         lib = lib.strip()
         if not launch.is_installed(lib):
-            if lib == 'rich':
-                launch.run(f'"{sys.executable}" -m pip install {lib}', desc=f"Installing Deforum requirement: {lib}", errdesc=f"Couldn't install {lib}")
-            else:
-                launch.run_pip(f"install {lib}", f"Deforum requirement: {lib}")
+            launch.run_pip(f"install {lib}", f"Deforum requirement: {lib}")
