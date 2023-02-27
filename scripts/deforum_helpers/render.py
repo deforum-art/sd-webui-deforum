74fc34e37ed4429c716daf880b37788c291049de
kabachuha <artemkhrapov2001@yandex.ru>
2023-02-27 11:56:23 +0300

    Revert "Revert "Merge pull request #339 from deforum-art/bad_apple!!""
    This reverts commit 483a973e0a9fc4197fe0a824598786fef52646ee.

--- scripts/deforum_helpers/render.py
+++ scripts/deforum_helpers/render.py
@@ -236,7 +236,7 @@ def render_animation(args, anim_args, video_args, parseq_args, loop_args, contro
 
                 if depth_model is not None:
                     assert(turbo_next_image is not None)
-                    depth = depth_model.predict(turbo_next_image, anim_args, root.half_precision)
+                    depth = depth_model.predict(turbo_next_image, anim_args.midas_weight, root.half_precision)
                 
                 if advance_prev:
                     turbo_prev_image, _ = anim_frame_warp(turbo_prev_image, args, anim_args, keys, tween_frame_idx, depth_model, depth=depth, device=root.device, half_precision=root.half_precision)
