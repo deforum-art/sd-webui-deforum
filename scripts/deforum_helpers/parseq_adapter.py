ebcb75a0a73e3563460dddbd25febbae44726b06
rewbs <robin@soal.org>
2023-03-02 06:36:33 +1100

    Fix 'UnboundLocalError: local variable 'parseqName' referenced before assignment'. Allow Parseq to delegate prompt management back to Deforum. Summary table in console showing who's doing what between Deforum & Parseq. Initial unit test.

--- scripts/deforum_helpers/parseq_adapter.py
+++ scripts/deforum_helpers/parseq_adapter.py
@@ -3,18 +3,20 @@ import json
 import logging
 import operator
 from operator import itemgetter
-
 import numpy as np
 import pandas as pd
 import requests
+import traceback
+import sys
 
 from .animation_key_frames import DeformAnimKeys
+from .rich import console
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
 class ParseqAnimKeys():
-    def __init__(self, parseq_args, anim_args):
+    def __init__(self, parseq_args, anim_args, video_args, mute=False):
 
         # Resolve manifest either directly from supplied value
         # or via supplied URL
@@ -45,20 +47,20 @@ class ParseqAnimKeys():
         self.default_anim_keys = DeformAnimKeys(anim_args)
         self.rendered_frames = self.parseq_json['rendered_frames']       
         self.max_frame = self.get_max('frame')
+        self.required_frames = anim_args.max_frames
+        # TODO these values are currently only used to emit a subtle warning. User must ensure the output FPS set in parseq
+        # matches the one set in Deforum to avoid unexpected results.
+        # In the future we may wish to override video_args.fps value with the one from parseq.
+        self.required_fps = video_args.fps
+        self.config_output_fps = self.parseq_json['options']['output_fps']
+
+        if not mute:
+            self.print_parseq_table()
+
         count_defined_frames = len(self.rendered_frames)        
         expected_defined_frames = self.max_frame+1 # frames are 0-indexed
-
-        self.required_frames = anim_args.max_frames
-
         if (expected_defined_frames != count_defined_frames): 
             logging.warning(f"There may be duplicated or missing frame data in the Parseq input: expected {expected_defined_frames} frames including frame 0 because the highest frame number is {self.max_frame}, but there are {count_defined_frames} frames defined.")
-
-        if (anim_args.max_frames > count_defined_frames):
-            logging.info(f"Parseq data defines {count_defined_frames} frames, but the requested animation is {anim_args.max_frames} frames. The last Parseq frame definition will be duplicated to match the expected frame count.")
-        if (anim_args.max_frames < count_defined_frames):
-            logging.info(f"Parseq data defines {count_defined_frames} frames, but the requested animation is {anim_args.max_frames} frames. The last Parseq frame definitions will be ignored.")
-        else:
-            logging.info(f"Parseq data defines {count_defined_frames} frames.")
 
         # Parseq treats input values as absolute values. So if you want to 
         # progressively rotate 180 degrees over 4 frames, you specify: 45, 90, 135, 180.
@@ -97,11 +99,37 @@ class ParseqAnimKeys():
         self.sigma_schedule_series = self.parseq_to_anim_series('antiblur_sigma')
         self.amount_schedule_series = self.parseq_to_anim_series('antiblur_amount')
         self.threshold_schedule_series = self.parseq_to_anim_series('antiblur_threshold')
-        
-        # Config:
-        # TODO this is currently ignored. User must ensure the output FPS set in parseq
-        # matches the one set in Deforum to avoid unexpected results.
-        self.config_output_fps = self.parseq_json['options']['output_fps']
+
+    def print_parseq_table(self):
+        from rich.table import Table
+        from rich import box
+        table = Table(padding=0, box=box.ROUNDED, show_lines=True)
+        table.add_column("", style="white bold")
+        table.add_column("Parseq", style="cyan")
+        table.add_column("Deforum", style="green")
+
+        table.add_row("Fields", '\n'.join(self.managed_fields()), '\n'.join(self.unmanaged_fields()))
+        table.add_row("Prompts", "✅" if self.manages_prompts() else "❌", "✅" if not self.manages_prompts() else "❌")
+        table.add_row("Frames", str(len(self.rendered_frames)), str(self.required_frames) + (" ⚠️" if self.required_frames != len(self.rendered_frames) else ""))
+        table.add_row("FPS", str(self.config_output_fps), str(self.required_fps) + (" ⚠️" if self.required_fps != self.config_output_fps else ""))
+
+        console.print("\nUse this table to validate your Parseq & Deforum setup:")
+        console.print(table)
+
+    def manages_prompts(self):
+        return 'deforum_prompt' in self.rendered_frames[0].keys()
+    
+    def managed_fields(self):
+        return [field for field in self.rendered_frames[0].keys()
+                            if (field not in ['frame', 'deforum_prompt']
+                                    and not field.endswith('_delta')
+                                    and not field.endswith('_pc'))]
+    
+    def unmanaged_fields(self):
+        managed_fields = self.managed_fields()
+        all_fields = [self.strip_suffixes(property) for property, _ in vars(self.default_anim_keys).items() if property not in ['fi'] and not property.startswith('_')]
+        return [field for field in all_fields if field not in managed_fields]
+
 
     def get_max(self, seriesName):
         return max(self.rendered_frames, key=itemgetter(seriesName))[seriesName]
@@ -112,7 +140,7 @@ class ParseqAnimKeys():
         # The Parseq contract is that the first frame (at least) must define values for all fields.
         try:
             if self.rendered_frames[0][seriesName] is not None:
-                logging.info(f"Found {seriesName} in first frame of Parseq data. Assuming it's defined.")
+                logging.debug(f"Found {seriesName} in first frame of Parseq data. Assuming it's defined.")
         except KeyError:
             return None
 
@@ -141,14 +169,7 @@ class ParseqAnimKeys():
             # No field with this name has been explicitly extracted from the JSON data.
             # It must be a new parameter. Let's see if it's in the raw JSON.
 
-            # parseq doesn't use _series, _schedule or _schedule_series suffixes in the
-            # JSON data - remove them.
-            strippableSuffixes = ['_series', '_schedule']
-            parseqName = name
-            while any(parseqName.endswith(suffix) for suffix in strippableSuffixes):
-                for suffix in strippableSuffixes:
-                    if parseqName.endswith(suffix):
-                        parseqName = parseqName[:-len(suffix)]            
+            parseqName = inst.strip_suffixes(name)
             
             # returns None if not defined in Parseq JSON data
             definedField = inst.parseq_to_anim_series(parseqName)
@@ -159,6 +180,18 @@ class ParseqAnimKeys():
         if (definedField is not None):
             return definedField
         else:
-            logging.info(f"Data for {name} not defined in Parseq data (looked for: '{parseqName}'). Falling back to standard Deforum values.")
+            logging.debug(f"Data for {name} not defined in Parseq data. Falling back to standard Deforum values.")
             return getattr(inst.default_anim_keys, name)
+
+    
+    # parseq doesn't use _series, _schedule or _schedule_series suffixes in the
+    # JSON data - remove them.        
+    def strip_suffixes(self, name):
+        strippableSuffixes = ['_series', '_schedule']
+        parseqName = name
+        while any(parseqName.endswith(suffix) for suffix in strippableSuffixes):
+            for suffix in strippableSuffixes:
+                if parseqName.endswith(suffix):
+                    parseqName = parseqName[:-len(suffix)]
+        return parseqName
 
