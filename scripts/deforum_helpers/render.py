23c0863ddf19e54b09a015953139ac58aa6a239d
Matisse <MatissesProjects@users.noreply.github.com>
2023-03-01 14:49:05 -0800

    attempt 2
    added s & max_f. Variable schedule planner, and prompt printout uses numbers instead of variables t/60 = .6 (at t=6)

--- scripts/deforum_helpers/render.py
+++ scripts/deforum_helpers/render.py
@@ -2,7 +2,9 @@ import os
 import json
 import pandas as pd
 import cv2
+import re
 import numpy as np
+import numexpr
 from PIL import Image, ImageOps
 from .rich import console
 
@@ -43,8 +45,8 @@ def render_animation(args, anim_args, video_args, parseq_args, loop_args, contro
     # use parseq if manifest is provided
     use_parseq = parseq_args.parseq_manifest != None and parseq_args.parseq_manifest.strip()
     # expand key frame strings to values
-    keys = DeformAnimKeys(anim_args) if not use_parseq else ParseqAnimKeys(parseq_args, anim_args)
-    loopSchedulesAndData = LooperAnimKeys(loop_args, anim_args)
+    keys = DeformAnimKeys(anim_args, args.seed) if not use_parseq else ParseqAnimKeys(parseq_args, anim_args)
+    loopSchedulesAndData = LooperAnimKeys(loop_args, anim_args, args.seed)
     # resume animation
     start_frame = 0
     if anim_args.resume_from_timestring:
@@ -88,8 +90,12 @@ def render_animation(args, anim_args, video_args, parseq_args, loop_args, contro
         prompt_series = keys.prompts
     else:
         prompt_series = pd.Series([np.nan for a in range(anim_args.max_frames)])
+        max_f = anim_args.max_frames - 1
         for i, prompt in animation_prompts.items():
-            prompt_series[int(i)] = prompt
+            if str(i).isdigit():
+                prompt_series[int(i)] = prompt
+            else:
+                prompt_series[int(numexpr.evaluate(i))] = prompt
         prompt_series = prompt_series.ffill().bfill()
 
     # check for video inits
@@ -230,7 +236,7 @@ def render_animation(args, anim_args, video_args, parseq_args, loop_args, contro
 
                 if depth_model is not None:
                     assert(turbo_next_image is not None)
-                    depth = depth_model.predict(turbo_next_image, anim_args.midas_weight, root.half_precision)
+                    depth = depth_model.predict(turbo_next_image, anim_args, root.half_precision)
                 
                 if advance_prev:
                     turbo_prev_image, _ = anim_frame_warp(turbo_prev_image, args, anim_args, keys, tween_frame_idx, depth_model, depth=depth, device=root.device, half_precision=root.half_precision)
@@ -373,8 +379,16 @@ def render_animation(args, anim_args, video_args, parseq_args, loop_args, contro
             args.seed_enable_extras = True
             args.subseed = int(keys.subseed_series[frame_idx])
             args.subseed_strength = keys.subseed_strength_series[frame_idx]
-            
-        prompt_to_print, *after_neg = args.prompt.strip().split("--neg")
+        
+        max_f = anim_args.max_frames - 1
+        prompt_split = re.split("`(.*?)`", args.prompt)
+        if len(prompt_split) > 1:
+            prompt_parsed = ''.join([prompt_split[value-1]+f'{numexpr.evaluate(prompt_split[value].replace("t",f"{frame_idx}").replace("max_f" , f"{max_f}"))}' for value in (range(1, len(prompt_split), 2))])
+        else:
+            prompt_parsed = args.prompt
+        prompt_parsed += ')' if ')' in prompt_split[-1] else "" # append last )
+
+        prompt_to_print, *after_neg = prompt_parsed.strip().split("--neg")
         prompt_to_print = prompt_to_print.strip()
         after_neg = "".join(after_neg).strip()
 
@@ -465,7 +479,7 @@ def render_animation(args, anim_args, video_args, parseq_args, loop_args, contro
                     sd_hijack.model_hijack.undo_hijack(sd_model)
                     devices.torch_gc()
                     depth_model.to(root.device)
-                depth = depth_model.predict(opencv_image, anim_args.midas_weight, root.half_precision)
+                depth = depth_model.predict(opencv_image, anim_args, root.half_precision)
                 depth_model.save(os.path.join(args.outdir, f"{args.timestring}_depth_{frame_idx:05}.png"), depth)
                 if cmd_opts.lowvram or cmd_opts.medvram:
                     depth_model.to('cpu')
@@ -489,8 +503,6 @@ def print_render_table(anim_args, keys, frame_idx):
     field_names += ["Tr X", "Tr Y"]
     if anim_args.animation_mode == '3D':
         field_names += ["Tr Z", "Ro X", "Ro Y", "Ro Z"]
-        if anim_args.aspect_ratio_schedule.replace(" ", "") != '0:(1)':
-            field_names += ["Asp. Ratio"]
     if anim_args.enable_perspective_flip:
         field_names += ["Pf T", "Pf P", "Pf G", "Pf F"]
     for field_name in field_names:
@@ -502,8 +514,6 @@ def print_render_table(anim_args, keys, frame_idx):
     rows += [str(keys.translation_x_series[frame_idx]),str(keys.translation_y_series[frame_idx])]
     if anim_args.animation_mode == '3D':
         rows += [str(keys.translation_z_series[frame_idx]),str(keys.rotation_3d_x_series[frame_idx]),str(keys.rotation_3d_y_series[frame_idx]),str(keys.rotation_3d_z_series[frame_idx])]
-        if anim_args.aspect_ratio_schedule.replace(" ", "") != '0:(1)':
-            rows += [str(keys.aspect_ratio_series[frame_idx])]
     if anim_args.enable_perspective_flip:
         rows +=[str(keys.perspective_flip_theta_series[frame_idx]), str(keys.perspective_flip_phi_series[frame_idx]), str(keys.perspective_flip_gamma_series[frame_idx]), str(keys.perspective_flip_fv_series[frame_idx])]
     table.add_row(*rows)
