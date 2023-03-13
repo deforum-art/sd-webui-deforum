490ccba11e237711f9b216b476a64b7fb1f84dd5
kabachuha <artemkhrapov2001@yandex.ru>
2023-03-14 00:41:28 +0300

    fix frame index math eval

--- scripts/deforum_helpers/animation_key_frames.py
+++ scripts/deforum_helpers/animation_key_frames.py
@@ -66,7 +66,7 @@ class FrameInterpolater():
         self.max_frames = max_frames
         self.seed = seed
 
-    def sanitize_value_for_check_is_number(self, value):
+    def sanitize_value(self, value):
         return value.replace("'","").replace('"',"").replace('(',"").replace(')',"")
 
     def get_inbetweens(self, key_frames, integer=False, interp_method='Linear', is_single_string = False):
@@ -77,13 +77,13 @@ class FrameInterpolater():
         for i in range(0, self.max_frames):
             if i in key_frames:
                 value = key_frames[i]
-                value_is_number = check_is_number(self.sanitize_value_for_check_is_number(value))
+                value_is_number = check_is_number(self.sanitize_value(value))
                 if value_is_number: # if it's only a number, leave the rest for the default interpolation
-                    key_frame_series[i] = self.sanitize_value_for_check_is_number(value)
+                    key_frame_series[i] = self.sanitize_value(value)
             if not value_is_number:
                 t = i
                 # workaround for values formatted like 0:("I am test") //used for sampler schedules
-                key_frame_series[i] = numexpr.evaluate(value) if not is_single_string else self.sanitize_value_for_check_is_number(value)
+                key_frame_series[i] = numexpr.evaluate(value) if not is_single_string else self.sanitize_value(value)
             elif is_single_string:# take previous string value and replicate it
                 key_frame_series[i] = key_frame_series[i-1]
         key_frame_series = key_frame_series.astype(float) if not is_single_string else key_frame_series # as string
@@ -110,7 +110,7 @@ class FrameInterpolater():
             frameParam = match_object.split(":")
             max_f = self.max_frames -1
             s = self.seed
-            frame = int(frameParam[0]) if check_is_number(frameParam[0].strip()) else int(numexpr.evaluate(frameParam[0].strip()))
+            frame = int(self.sanitize_value(frameParam[0])) if check_is_number(self.sanitize_value(frameParam[0].strip())) else int(numexpr.evaluate(frameParam[0].strip().replace("'","").replace('"',"")[::-1].replace("'","").replace('"',"")[::-1]))
             frames[frame] = frameParam[1].strip()
         if frames == {} and len(string) != 0:
             raise RuntimeError('Key Frame string not correctly formatted')
