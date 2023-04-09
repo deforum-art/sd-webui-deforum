6660cab05327c57dc61e9e7d6916ed828a4477dc
reallybigname <reallybigname@gmail.com>
2023-04-09 07:31:49 -0700

    made overlay masks with online images
    now loads init_image and mask_file using load_image function
    
    also fixed issue where you needed an init_image
      - passes image through if no init image

--- scripts/deforum_helpers/masks.py
+++ scripts/deforum_helpers/masks.py
@@ -4,6 +4,7 @@ import gc
 import numpy as np
 from PIL import Image, ImageOps
 from .video_audio_utilities import get_frame_name
+from .load_images import load_image
 
 def do_overlay_mask(args, anim_args, img, frame_idx, is_bgr_array=False):
     if is_bgr_array:
@@ -14,8 +15,11 @@ def do_overlay_mask(args, anim_args, img, frame_idx, is_bgr_array=False):
         current_mask = Image.open(os.path.join(args.outdir, 'maskframes', get_frame_name(anim_args.video_mask_path) + f"{frame_idx+1:09}.jpg"))
         current_frame = Image.open(os.path.join(args.outdir, 'inputframes', get_frame_name(anim_args.video_init_path) + f"{frame_idx+1:09}.jpg"))
     elif args.use_mask:
-        current_mask = Image.open(args.mask_file)
-        current_frame = Image.open(args.init_image)
+        current_mask = load_image(args.mask_file)
+        if args.init_image is None:
+            current_frame = img
+        else:
+            current_frame = load_image(args.init_image)
 
     current_mask = current_mask.resize((args.W, args.H), Image.LANCZOS)
     current_frame = current_frame.resize((args.W, args.H), Image.LANCZOS)
