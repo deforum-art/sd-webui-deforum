3cfa2c0744f6ef30999e32269e40372c60d5bae5
hithereai <121192995+hithereai@users.noreply.github.com>
2023-04-11 00:42:39 +0300

    add .srt subtitles generation info support

--- scripts/deforum.py
+++ scripts/deforum.py
@@ -36,6 +36,7 @@ import modules.shared as shared
 from modules.shared import opts, cmd_opts, state
 from modules.ui import create_output_panel, plaintext_to_html, wrap_gradio_call
 from types import SimpleNamespace
+from deforum_helpers.subtitle_handler import get_user_values
 
 DEBUG_MODE = opts.data.get("deforum_debug_mode_enabled", False)
 
@@ -330,9 +331,8 @@ def on_ui_tabs():
     return [(deforum_interface, "Deforum", "deforum_interface")]
 
 def on_ui_settings():
+    srt_ui_params = get_user_values()
     section = ('deforum', "Deforum")
-    # shared.opts.add_option("deforum_start_with_more_info_enabled", shared.OptionInfo(
-        # True, "Start deforum with 'info mode' enabled", gr.Checkbox, {"interactive": True}, section=section))   
     shared.opts.add_option("deforum_keep_3d_models_in_vram", shared.OptionInfo(False, "Keep 3D models in VRAM between runs", gr.Checkbox, {"interactive": True, "visible": True if not (cmd_opts.lowvram or cmd_opts.medvram) else False}, section=section))
     shared.opts.add_option("deforum_enable_persistent_settings", shared.OptionInfo(False, "Keep settings persistent upon relaunch of webui", gr.Checkbox, {"interactive": True}, section=section))
     shared.opts.add_option("deforum_persistent_settings_path", shared.OptionInfo("models/Deforum/deforum_persistent_settings.txt", "Path for saving your persistent settings file:", section=section))
@@ -340,6 +340,8 @@ def on_ui_settings():
     shared.opts.add_option("deforum_ffmpeg_crf", shared.OptionInfo(17, "FFmpeg CRF value", gr.Slider, {"interactive": True, "minimum": 0, "maximum": 51}, section=section))
     shared.opts.add_option("deforum_ffmpeg_preset", shared.OptionInfo('slow', "FFmpeg Preset", gr.Dropdown, {"interactive": True, "choices": ['veryslow', 'slower', 'slow', 'medium', 'fast', 'faster', 'veryfast', 'superfast', 'ultrafast']}, section=section))
     shared.opts.add_option("deforum_debug_mode_enabled", shared.OptionInfo(False, "Enable Dev mode - adds extra reporting in console", gr.Checkbox, {"interactive": True}, section=section))
+    shared.opts.add_option("deforum_save_gen_info_as_srt", shared.OptionInfo(False, "Save an .srt (subtitles) file with the generation info along with each animation", gr.Checkbox, {"interactive": True}, section=section))  
+    shared.opts.add_option("deforum_save_gen_info_as_srt_params", shared.OptionInfo(['Noise Schedule'], "Choose which animation params are to be saved to the .srt file (Frame # and Seed will always be saved):", gr.CheckboxGroup, {"interactive": True, "choices": srt_ui_params}, section=section))  
         
 script_callbacks.on_ui_tabs(on_ui_tabs)
 script_callbacks.on_ui_settings(on_ui_settings)
