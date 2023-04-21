18c5c5aab74cce312f13b8421fc7f93db2a8948a
hithereai <121192995+hithereai@users.noreply.github.com>
2023-04-21 22:32:27 +0300

    set raft quality default to 50 instead of 12, fix optical flow cadence always being Farnback

--- scripts/deforum_helpers/RAFT.py
+++ scripts/deforum_helpers/RAFT.py
@@ -9,16 +9,16 @@ class RAFT:
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model = raft_large(weights=weights, progress=False).to(self.device).eval()
 
-    def predict(self, image1, image2):
+    def predict(self, image1, image2, num_flow_updates:int = 50):
         img1 = F.to_tensor(image1)
         img2 = F.to_tensor(image2)
         img1_batch, img2_batch = img1.unsqueeze(0), img2.unsqueeze(0)
         img1_batch, img2_batch = self.transforms(img1_batch, img2_batch)
 
         with torch.no_grad():
-            flow = self.model(img1_batch.to(self.device), img2_batch.to(self.device))[-1].cpu().numpy()[0]
+            flow = self.model(image1 = img1_batch.to(self.device), image2 = img2_batch.to(self.device), num_flow_updates = num_flow_updates)[-1].cpu().numpy()[0]
 
         return flow
-        
+
     def delete_model(self):
         del self.model
