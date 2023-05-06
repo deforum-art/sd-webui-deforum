a68ac64f0e8ed574d49540e2d3577f57258a8659
hithereai <121192995+hithereai@users.noreply.github.com>
2023-05-06 23:24:18 +0300

    Delete __init__.py
    we don't need it

--- scripts/deforum_helpers/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-from .save_images import save_samples, get_output_folder
-from .depth import DepthModel
-from .prompt import sanitize
-from .animation import construct_RotationMatrixHomogenous, getRotationMatrixManual, getPoints_for_PerspectiveTranformEstimation, warpMatrix, anim_frame_warp_2d, anim_frame_warp_3d
-from .generate import add_noise, load_img, load_mask_latent, prepare_mask
-"""
