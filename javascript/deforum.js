636fb987cfd86c6ea012521c28b57f58ace4e3d7
hithereai <121192995+hithereai@users.noreply.github.com>
2023-05-18 00:13:23 +0300

    clean

--- javascript/deforum.js
+++ javascript/deforum.js
@@ -1,5 +1,4 @@
 function submit_deforum(){
-	// alert('Hello, Deforum!')
     rememberGallerySelection('deforum_gallery')
     showSubmitButtons('deforum', false)
 
@@ -11,7 +10,6 @@ function submit_deforum(){
     var res = create_submit_args(arguments)
 
     res[0] = id
-	// res[1] = get_tab_index('deforum')
 
     return res
 }
