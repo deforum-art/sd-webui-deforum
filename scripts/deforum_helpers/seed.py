53867ac85a74701bfadff9dd8f6d9b4e3a2b6e5a
hithereai <121192995+hithereai@users.noreply.github.com>
2023-05-18 14:04:46 +0300

    shorten some long code

--- scripts/deforum_helpers/seed.py
+++ scripts/deforum_helpers/seed.py
@@ -1,29 +1,18 @@
 import random
 from modules.shared import opts
-
-DEBUG_MODE = opts.data.get("deforum_debug_mode_enabled", False)
-
+   
 def next_seed(args):
     if args.seed_behavior == 'iter':
-        if args.seed_internal % args.seed_iter_N == 0:
-            args.seed += 1
+        args.seed += 1 if args.seed_internal % args.seed_iter_N == 0 else 0
         args.seed_internal += 1
     elif args.seed_behavior == 'ladder':
-        if args.seed_internal == 0:
-            args.seed += 2
-            args.seed_internal = 1
-        else:
-            args.seed -= 1
-            args.seed_internal = 0
+        args.seed += 2 if args.seed_internal == 0 else -1
+        args.seed_internal = 1 if args.seed_internal == 0 else 0
     elif args.seed_behavior == 'alternate':
-        if args.seed_internal == 0:
-            args.seed += 1
-            args.seed_internal = 1
-        else:
-            args.seed -= 1
-            args.seed_internal = 0
+        args.seed += 1 if args.seed_internal == 0 else -1
+        args.seed_internal = 1 if args.seed_internal == 0 else 0
     elif args.seed_behavior == 'fixed':
-        pass # always keep seed the same
+        pass  # always keep seed the same
     else:
         args.seed = random.randint(0, 2**32 - 1)
     return args.seed
