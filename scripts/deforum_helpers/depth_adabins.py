ecf0769454d7234be40976354555a1b1ac3cfc2a
hithereai <121192995+hithereai@users.noreply.github.com>
2023-05-19 11:39:22 +0300

    add missing import to fix adabins when resized
    Closes https://github.com/deforum-art/sd-webui-deforum/issues/725

--- scripts/deforum_helpers/depth_adabins.py
+++ scripts/deforum_helpers/depth_adabins.py
@@ -3,6 +3,7 @@ import torch
 from infer import InferenceHelper
 import numpy as np
 from PIL import Image
+import torchvision.transforms.functional as TF
 from .general_utils import download_file_with_checksum
 
 class AdaBinsModel:
