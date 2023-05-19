6cdfd489433ccea0ca66249c11c565bfc31fe838
hithereai <121192995+hithereai@users.noreply.github.com>
2023-05-19 19:18:52 +0300

    Update generate.py

--- scripts/deforum_helpers/generate.py
+++ scripts/deforum_helpers/generate.py
@@ -19,8 +19,8 @@ from .prompt import check_is_number
 def load_mask_latent(mask_input, shape):
     # mask_input (str or PIL Image.Image): Path to the mask image or a PIL Image object
     # shape (list-like len(4)): shape of the image to match, usually latent_image.shape
-    
-    if isinstance(mask_input, str): # mask input is probably a file name
+
+    if isinstance(mask_input, str):  # mask input is probably a file name
         if mask_input.startswith('http://') or mask_input.startswith('https://'):
             mask_image = Image.open(requests.get(mask_input, stream=True).raw).convert('RGBA')
         else:
@@ -49,13 +49,13 @@ def pairwise_repl(iterable):
     next(b, None)
     return zip(a, b)
 
-def generate(args, keys, anim_args, loop_args, controlnet_args, root, frame = 0, sampler_name=None):
+def generate(args, keys, anim_args, loop_args, controlnet_args, root, frame=0, sampler_name=None):
     if state.interrupted:
         return None
-        
+
     if args.reroll_blank_frames == 'ignore':
         return generate_inner(args, keys, anim_args, loop_args, controlnet_args, root, frame, sampler_name)
-    
+
     image, caught_vae_exception = generate_with_nans_check(args, keys, anim_args, loop_args, controlnet_args, root, frame, sampler_name)
 
     if caught_vae_exception or not image.getbbox():
@@ -79,7 +79,7 @@ def generate(args, keys, anim_args, loop_args, controlnet_args, root, frame = 0,
             return None
     return image
 
-def generate_with_nans_check(args, keys, anim_args, loop_args, controlnet_args, root, frame = 0, sampler_name=None):
+def generate_with_nans_check(args, keys, anim_args, loop_args, controlnet_args, root, frame=0, sampler_name=None):
     if cmd_opts.disable_nan_check:
         image = generate_inner(args, keys, anim_args, loop_args, controlnet_args, root, frame, sampler_name)
     else:
@@ -93,11 +93,11 @@ def generate_with_nans_check(args, keys, anim_args, loop_args, controlnet_args, 
                 raise e
     return image, False
 
-def generate_inner(args, keys, anim_args, loop_args, controlnet_args, root, frame = 0, sampler_name=None):
+def generate_inner(args, keys, anim_args, loop_args, controlnet_args, root, frame=0, sampler_name=None):
     # Setup the pipeline
     p = get_webui_sd_pipeline(args, root)
     p.prompt, p.negative_prompt = split_weighted_subprompts(args.prompt, frame, anim_args.max_frames)
-    
+
     if not args.use_init and args.strength > 0 and args.strength_0_no_init:
         args.strength = 0
     processed = None
@@ -105,7 +105,7 @@ def generate_inner(args, keys, anim_args, loop_args, controlnet_args, root, fram
     init_image = None
     image_init0 = None
 
-    if loop_args.use_looper and anim_args.animation_mode in ['2D','3D']:
+    if loop_args.use_looper and anim_args.animation_mode in ['2D', '3D']:
         args.strength = loop_args.imageStrength
         tweeningFrames = loop_args.tweeningFrameSchedule
         blendFactor = .07
@@ -115,52 +115,52 @@ def generate_inner(args, keys, anim_args, loop_args, controlnet_args, root, fram
         parsedImages = {}
         frameToChoose = 0
         max_f = anim_args.max_frames - 1
-        
+
         for key, value in jsonImages.items():
-            if check_is_number(key):# default case 0:(1 + t %5), 30:(5-t%2)
+            if check_is_number(key):  # default case 0:(1 + t %5), 30:(5-t%2)
                 parsedImages[key] = value
-            else:# math on the left hand side case 0:(1 + t %5), maxKeyframes/2:(5-t%2)
+            else:  # math on the left hand side case 0:(1 + t %5), maxKeyframes/2:(5-t%2)
                 parsedImages[int(numexpr.evaluate(key))] = value
 
         framesToImageSwapOn = list(map(int, list(parsedImages.keys())))
 
         for swappingFrame in framesToImageSwapOn[1:]:
             frameToChoose += (frame >= int(swappingFrame))
-        
-        #find which frame to do our swapping on for tweening
+
+        # find which frame to do our swapping on for tweening
         skipFrame = 25
         for fs, fe in pairwise_repl(framesToImageSwapOn):
             if fs <= frame <= fe:
                 skipFrame = fe - fs
 
-        if frame % skipFrame <= tweeningFrames: # number of tweening frames
-            blendFactor = loop_args.blendFactorMax - loop_args.blendFactorSlope*math.cos((frame % tweeningFrames) / (tweeningFrames / 2))
+        if frame % skipFrame <= tweeningFrames:  # number of tweening frames
+            blendFactor = loop_args.blendFactorMax - loop_args.blendFactorSlope * math.cos((frame % tweeningFrames) / (tweeningFrames / 2))
         init_image2, _ = load_img(list(jsonImages.values())[frameToChoose],
-                                shape=(args.W, args.H),
-                                use_alpha_as_mask=args.use_alpha_as_mask)
+                                  shape=(args.W, args.H),
+                                  use_alpha_as_mask=args.use_alpha_as_mask)
         image_init0 = list(jsonImages.values())[0]
-            
-    else: # they passed in a single init image
+
+    else:  # they passed in a single init image
         image_init0 = args.init_image
 
-    available_samplers = { 
-        'euler a':'Euler a',
-        'euler':'Euler',
-        'lms':'LMS',
-        'heun':'Heun',
-        'dpm2':'DPM2',
-        'dpm2 a':'DPM2 a',
-        'dpm++ 2s a':'DPM++ 2S a',
-        'dpm++ 2m':'DPM++ 2M',
-        'dpm++ sde':'DPM++ SDE',
-        'dpm fast':'DPM fast',
-        'dpm adaptive':'DPM adaptive',
-        'lms karras':'LMS Karras' ,
-        'dpm2 karras':'DPM2 Karras',
-        'dpm2 a karras':'DPM2 a Karras',
-        'dpm++ 2s a karras':'DPM++ 2S a Karras',
-        'dpm++ 2m karras':'DPM++ 2M Karras',
-        'dpm++ sde karras':'DPM++ SDE Karras'
+    available_samplers = {
+        'euler a': 'Euler a',
+        'euler': 'Euler',
+        'lms': 'LMS',
+        'heun': 'Heun',
+        'dpm2': 'DPM2',
+        'dpm2 a': 'DPM2 a',
+        'dpm++ 2s a': 'DPM++ 2S a',
+        'dpm++ 2m': 'DPM++ 2M',
+        'dpm++ sde': 'DPM++ SDE',
+        'dpm fast': 'DPM fast',
+        'dpm adaptive': 'DPM adaptive',
+        'lms karras': 'LMS Karras',
+        'dpm2 karras': 'DPM2 Karras',
+        'dpm2 a karras': 'DPM2 a Karras',
+        'dpm++ 2s a karras': 'DPM++ 2S a Karras',
+        'dpm++ 2m karras': 'DPM++ 2M Karras',
+        'dpm++ sde karras': 'DPM++ SDE Karras'
     }
     if sampler_name is not None:
         if sampler_name in available_samplers.keys():
@@ -173,114 +173,115 @@ def generate_inner(args, keys, anim_args, loop_args, controlnet_args, root, fram
         if info is None:
             raise RuntimeError(f"Unknown checkpoint: {args.checkpoint}")
         sd_models.reload_model_weights(info=info)
-    
+
     if args.init_sample is not None:
         # TODO: cleanup init_sample remains later
         img = args.init_sample
         init_image = img
         image_init0 = img
-        if loop_args.use_looper and isJson(loop_args.imagesToKeyframe) and anim_args.animation_mode in ['2D','3D']:
+        if loop_args.use_looper and isJson(loop_args.imagesToKeyframe) and anim_args.animation_mode in ['2D', '3D']:
             init_image = Image.blend(init_image, init_image2, blendFactor)
             correction_colors = Image.blend(init_image, init_image2, colorCorrectionFactor)
             p.color_corrections = [processing.setup_color_correction(correction_colors)]
 
     # this is the first pass
-    elif (loop_args.use_looper and anim_args.animation_mode in ['2D','3D']) or (args.use_init and ((args.init_image != None and args.init_image != ''))):
-        init_image, mask_image = load_img(image_init0, # initial init image
-                                          shape=(args.W, args.H),  
+    elif (loop_args.use_looper and anim_args.animation_mode in ['2D', '3D']) or (args.use_init and ((args.init_image != None and args.init_image != ''))):
+        init_image, mask_image = load_img(image_init0,  # initial init image
+                                          shape=(args.W, args.H),
                                           use_alpha_as_mask=args.use_alpha_as_mask)
-                                          
+
     else:
-        
+
         if anim_args.animation_mode != 'Interpolation':
             print(f"Not using an init image (doing pure txt2img)")
         p_txt = StableDiffusionProcessingTxt2Img(
-                sd_model=sd_model,
-                outpath_samples=root.tmp_deforum_run_duplicated_folder,
-                outpath_grids=root.tmp_deforum_run_duplicated_folder,
-                prompt=p.prompt,
-                styles=p.styles,
-                negative_prompt=p.negative_prompt,
-                seed=p.seed,
-                subseed=p.subseed,
-                subseed_strength=p.subseed_strength,
-                seed_resize_from_h=p.seed_resize_from_h,
-                seed_resize_from_w=p.seed_resize_from_w,
-                sampler_name=p.sampler_name,
-                batch_size=p.batch_size,
-                n_iter=p.n_iter,
-                steps=p.steps,
-                cfg_scale=p.cfg_scale,
-                width=p.width,
-                height=p.height,
-                restore_faces=p.restore_faces,
-                tiling=p.tiling,
-                enable_hr=None,
-                denoising_strength=None,
-            )
-            
-        print_combined_table(args, anim_args, p_txt, keys, frame) # print dynamic table to cli
+            sd_model=sd_model,
+            outpath_samples=root.tmp_deforum_run_duplicated_folder,
+            outpath_grids=root.tmp_deforum_run_duplicated_folder,
+            prompt=p.prompt,
+            styles=p.styles,
+            negative_prompt=p.negative_prompt,
+            seed=p.seed,
+            subseed=p.subseed,
+            subseed_strength=p.subseed_strength,
+            seed_resize_from_h=p.seed_resize_from_h,
+            seed_resize_from_w=p.seed_resize_from_w,
+            sampler_name=p.sampler_name,
+            batch_size=p.batch_size,
+            n_iter=p.n_iter,
+            steps=p.steps,
+            cfg_scale=p.cfg_scale,
+            width=p.width,
+            height=p.height,
+            restore_faces=p.restore_faces,
+            tiling=p.tiling,
+            enable_hr=None,
+            denoising_strength=None,
+        )
+
+        print_combined_table(args, anim_args, p_txt, keys, frame)  # print dynamic table to cli
 
         if is_controlnet_enabled(controlnet_args):
             process_with_controlnet(p_txt, args, anim_args, loop_args, controlnet_args, root, is_img2img=False, frame_idx=frame)
-    
+
         processed = processing.process_images(p_txt)
 
     if processed is None:
         # Mask functions
         if args.use_mask:
             mask_image = args.mask_image
-            mask = prepare_mask(args.mask_file if mask_image is None else mask_image, 
+            mask = prepare_mask(args.mask_file if mask_image is None else mask_image,
                                 (args.W, args.H),
-                                args.mask_contrast_adjust, 
+                                args.mask_contrast_adjust,
                                 args.mask_brightness_adjust)
             p.inpainting_mask_invert = args.invert_mask
-            p.inpainting_fill = args.fill 
-            p.inpaint_full_res= args.full_res_mask 
+            p.inpainting_fill = args.fill
+            p.inpaint_full_res = args.full_res_mask
             p.inpaint_full_res_padding = args.full_res_mask_padding
-            #prevent loaded mask from throwing errors in Image operations if completely black and crop and resize in webui pipeline
-            #doing this after contrast and brightness adjustments to ensure that mask is not passed as black or blank
+            # prevent loaded mask from throwing errors in Image operations if completely black and crop and resize in webui pipeline
+            # doing this after contrast and brightness adjustments to ensure that mask is not passed as black or blank
             mask = check_mask_for_errors(mask, args.invert_mask)
             args.noise_mask = mask
         else:
             mask = None
 
-        assert not ( (mask is not None and args.use_mask and args.overlay_mask) and (args.init_sample is None and init_image is None)), "Need an init image when use_mask == True and overlay_mask == True"
-        
+        assert not ((mask is not None and args.use_mask and args.overlay_mask) and (
+                    args.init_sample is None and init_image is None)), "Need an init image when use_mask == True and overlay_mask == True"
+
         p.init_images = [init_image]
         p.image_mask = mask
         p.image_cfg_scale = args.pix2pix_img_cfg_scale
-        
-        print_combined_table(args, anim_args, p, keys, frame) # print dynamic table to cli
-       
+
+        print_combined_table(args, anim_args, p, keys, frame)  # print dynamic table to cli
+
         if is_controlnet_enabled(controlnet_args):
             process_with_controlnet(p, args, anim_args, loop_args, controlnet_args, root, is_img2img=True, frame_idx=frame)
-    
+
         processed = processing.process_images(p)
-    
+
     if root.initial_info == None:
         root.initial_info = processed.info
-        
+
     if root.first_frame == None:
         root.first_frame = processed.images[0]
-        
+
     results = processed.images[0]
-    
+
     return results
-    
+
 def print_combined_table(args, anim_args, p, keys, frame_idx):
     from rich.table import Table
     from rich import box
 
     table = Table(padding=0, box=box.ROUNDED)
-    
+
     field_names1 = ["Steps", "CFG"]
     if anim_args.animation_mode != 'Interpolation':
         field_names1.append("Denoise")
     field_names1 += ["Subseed", "Subs. str"] * (anim_args.enable_subseed_scheduling)
     field_names1 += ["Sampler"] * anim_args.enable_sampler_scheduling
     field_names1 += ["Checkpoint"] * anim_args.enable_checkpoint_scheduling
-    
+
     for field_name in field_names1:
         table.add_column(field_name, justify="center")
 
@@ -288,10 +289,10 @@ def print_combined_table(args, anim_args, p, keys, frame_idx):
     if anim_args.animation_mode != 'Interpolation':
         rows1.append(f"{p.denoising_strength:.5g}" if p.denoising_strength is not None else "None")
 
-    rows1 += [str(p.subseed), f"{p.subseed_strength:.5g}"] * (anim_args.enable_subseed_scheduling)
+    rows1 += [str(p.subseed), f"{p.subseed_strength:.5g}"] * anim_args.enable_subseed_scheduling
     rows1 += [p.sampler_name] * anim_args.enable_sampler_scheduling
     rows1 += [str(args.checkpoint)] * anim_args.enable_checkpoint_scheduling
-    
+
     rows2 = []
     if anim_args.animation_mode not in ['Video Input', 'Interpolation']:
         if anim_args.animation_mode == '2D':
@@ -305,14 +306,14 @@ def print_combined_table(args, anim_args, p, keys, frame_idx):
                 field_names2 += ["Asp. Ratio"]
         if anim_args.enable_perspective_flip:
             field_names2 += ["Pf T", "Pf P", "Pf G", "Pf F"]
-        
+
         for field_name in field_names2:
             table.add_column(field_name, justify="center")
 
         if anim_args.animation_mode == '2D':
             rows2 += [f"{keys.angle_series[frame_idx]:.5g}", f"{keys.zoom_series[frame_idx]:.5g}"]
         rows2 += [f"{keys.translation_x_series[frame_idx]:.5g}", f"{keys.translation_y_series[frame_idx]:.5g}"]
- 
+
         if anim_args.animation_mode == '3D':
             rows2 += [f"{keys.translation_z_series[frame_idx]:.5g}", f"{keys.rotation_3d_x_series[frame_idx]:.5g}",
                       f"{keys.rotation_3d_y_series[frame_idx]:.5g}", f"{keys.rotation_3d_z_series[frame_idx]:.5g}"]
