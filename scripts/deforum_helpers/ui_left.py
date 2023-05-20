4bf6a83e006a56a914474cf26aa4aa68670992e8
hithereai <121192995+hithereai@users.noreply.github.com>
2023-05-20 17:23:50 +0300

    Update ui_left.py

--- scripts/deforum_helpers/ui_left.py
+++ scripts/deforum_helpers/ui_left.py
@@ -18,9 +18,10 @@ def set_arg_lists():
 
 def setup_deforum_left_side_ui():
     d, da, dp, dv, dr, dloopArgs = set_arg_lists()
-    # MAIN (TOP) EXTENSION INFO ACCORD
+    # set up main info accordion on top of the UI
     with gr.Accordion("Info, Links and Help", open=False, elem_id='main_top_info_accord'):
         gr.HTML(value=get_gradio_html('main'))
+    # show button to hide/ show gradio's info texts for each element in the UI
     with gr.Row(variant='compact'):
         show_info_on_ui = gr.Checkbox(label="Show more info", value=d.show_info_on_ui, interactive=True)
     with gr.Blocks():
