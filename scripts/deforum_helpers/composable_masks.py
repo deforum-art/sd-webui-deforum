5f075c810ad475c9503ed28ea6684120946afcce
kabachuha <artemkhrapov2001@yandex.ru>
2023-08-12 09:46:20 +0300

    add License notices

--- scripts/deforum_helpers/composable_masks.py
+++ scripts/deforum_helpers/composable_masks.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2023 Deforum LLC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+# Contact the authors: https://deforum.github.io/
+
 # At the moment there are three types of masks: mask from variable, file mask and word mask
 # Variable masks include video_mask (which can be set to auto-generated human masks) and everywhere
 # They are put in {}-brackets
