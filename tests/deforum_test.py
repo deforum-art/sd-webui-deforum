c9335770fce52d83f2b08503368fe2b1370a3aba
rewbs <robin@soal.org>
2023-09-20 10:56:15 +1000

    Add tests with/without new parseq override option.

--- tests/deforum_test.py
+++ tests/deforum_test.py
@@ -14,16 +14,20 @@
 
 # Contact the authors: https://deforum.github.io/
 
-import os
 import json
-from scripts.deforum_api_models import DeforumJobStatus, DeforumJobStatusCategory, DeforumJobPhase
+import math
+import os
+from pathlib import Path
+
 import requests
 from moviepy.editor import VideoFileClip
-import glob
-from pathlib import Path
-from utils import wait_for_job_to_complete, wait_for_job_to_enter_phase, wait_for_job_to_enter_status, API_BASE_URL
+from utils import (API_BASE_URL, wait_for_job_to_complete,
+                   wait_for_job_to_enter_phase, wait_for_job_to_enter_status)
 
+from scripts.deforum_api_models import (DeforumJobPhase, DeforumJobStatus,
+                                        DeforumJobStatusCategory)
 from scripts.deforum_helpers.subtitle_handler import get_user_values
+
 
 def test_simple_settings(snapshot):
     with open('tests/testdata/simple.input_settings.txt', 'r') as settings_file:
@@ -105,14 +109,15 @@ def test_3d_mode(snapshot):
     assert clip.size == [deforum_settings['W'], deforum_settings['H']] , "Video dimensions are not as expected"
 
 
-def test_with_parseq_inline(snapshot):
+def test_with_parseq_inline_without_overrides(snapshot):
     with open('tests/testdata/simple.input_settings.txt', 'r') as settings_file:
         deforum_settings = json.load(settings_file)
 
     with open('tests/testdata/parseq.json', 'r') as parseq_file:
         parseq_data = json.load(parseq_file)
-        
+       
     deforum_settings['parseq_manifest'] = json.dumps(parseq_data)
+    deforum_settings["parseq_non_schedule_overrides"] = False
     
     response = requests.post(API_BASE_URL+"/batches", json={
         "deforum_settings":[deforum_settings],
@@ -139,6 +144,42 @@ def test_with_parseq_inline(snapshot):
     assert clip.duration * clip.fps == deforum_settings['max_frames'] , "Video frame count does not match input settings"
     assert clip.size == [deforum_settings['W'], deforum_settings['H']] , "Video dimensions are not as expected"
 
+
+def test_with_parseq_inline_with_overrides(snapshot):
+    with open('tests/testdata/simple.input_settings.txt', 'r') as settings_file:
+        deforum_settings = json.load(settings_file)
+
+    with open('tests/testdata/parseq.json', 'r') as parseq_file:
+        parseq_data = json.load(parseq_file)
+       
+    deforum_settings['parseq_manifest'] = json.dumps(parseq_data)
+    deforum_settings["parseq_non_schedule_overrides"] = True
+    
+    response = requests.post(API_BASE_URL+"/batches", json={
+        "deforum_settings":[deforum_settings],
+        "options_overrides": {
+            "deforum_save_gen_info_as_srt": True,
+            "deforum_save_gen_info_as_srt_params": get_user_values(),
+            }
+        })
+    response.raise_for_status()
+    job_id = response.json()["job_ids"][0]
+    jobStatus = wait_for_job_to_complete(job_id)
+
+    assert jobStatus.status == DeforumJobStatusCategory.SUCCEEDED, f"Job {job_id} failed: {jobStatus}"
+
+    # Ensure parameters used at each frame have not regressed
+    srt_filenname =  os.path.join(jobStatus.outdir, f"{jobStatus.timestring}.srt")
+    with open(srt_filenname, 'r') as srt_file:
+        assert srt_file.read() == snapshot
+
+    # Ensure video format is as expected
+    video_filename =  os.path.join(jobStatus.outdir, f"{jobStatus.timestring}.mp4")
+    clip = VideoFileClip(video_filename)
+    expected_frame_count = len(parseq_data['rendered_frames'])
+    assert clip.fps == parseq_data['options']['output_fps'] , "Video FPS does not match input settings"
+    assert math.ceil(clip.duration * clip.fps) == expected_frame_count, "Video frame count does not match input settings"
+    assert clip.size == [deforum_settings['W'], deforum_settings['H']] , "Video dimensions are not as expected"
 
 # def test_with_parseq_url():
 
