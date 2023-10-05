73098172261486371ad0ac6bd5a2a2db6541cf2d
kabachuha <artemkhrapov2001@yandex.ru>
2023-10-05 22:23:15 +0300

    add animatediff params to UI

--- scripts/deforum_helpers/deforum_animatediff.py
+++ scripts/deforum_helpers/deforum_animatediff.py
@@ -68,9 +68,10 @@ def animatediff_component_names():
         return []
 
     return [f'animatediff_{i}' for i in [
-        'enabled', 'model', 'video_length', 'fps', 'loop_number',
-        'closed_loop', 'stride', 'overlap', 'interp', 'interp_x', 'reverse', 
-        'latent_power', 'latent_scale', 'threshold_b', 'resize_mode', 'control_mode', 'loopback_mode'
+        'enabled', 'model', 'motion_lora_schedule',
+        'window_length', # sliding window length (context batch size)
+        'window_overlap', # how much do the contexts overlap. if -1, then batch_size // 4
+        'latent_power', 'latent_scale',
     ]]
 
 def setup_animatediff_ui_raw():
@@ -99,50 +100,31 @@ def setup_animatediff_ui_raw():
 
     refresh_symbol = '\U0001f504'  # 🔄
     switch_values_symbol = '\U000021C5'  # ⇅
-    model_dropdowns = []
     infotext_fields = []
 
     # TODO: unwrap
     def create_model_in_tab_ui(cn_id):
         with gr.Row():
-            enabled = gr.Checkbox(label="Enable", value=False, interactive=True)
-            pixel_perfect = gr.Checkbox(label="Pixel Perfect", value=False, visible=False, interactive=True)
-            low_vram = gr.Checkbox(label="Low VRAM", value=False, visible=False, interactive=True)
-            overwrite_frames = gr.Checkbox(label='Overwrite input frames', value=True, visible=False, interactive=True)
+            enabled = gr.Checkbox(label="Enable AnimateDiff", value=False, interactive=True)
         with gr.Row(visible=False) as mod_row:
-            model = gr.Dropdown(cn_models, label=f"Model", value="None", interactive=True)
+            model = gr.Dropdown(cn_models, label=f"Motion module", value="None", interactive=True, tooltip="Choose which motion module will be injected into the generation process.")
             refresh_models = ToolButton(value=refresh_symbol)
             refresh_models.click(refresh_all_models, model, model)
-        with gr.Row(visible=False) as weight_row:
-            weight = gr.Textbox(label="Weight schedule", lines=1, value='0:(1)', interactive=True)
-        with gr.Row(visible=False) as start_cs_row:
-            guidance_start = gr.Textbox(label="Starting Control Step schedule", lines=1, value='0:(0.0)', interactive=True)
-        with gr.Row(visible=False) as end_cs_row:
-            guidance_end = gr.Textbox(label="Ending Control Step schedule", lines=1, value='0:(1.0)', interactive=True)
-            model_dropdowns.append(model)
-        with gr.Column(visible=False) as advanced_column:
-            processor_res = gr.Slider(label="Annotator resolution", value=64, minimum=64, maximum=2048, interactive=False)
-            threshold_a = gr.Slider(label="Threshold A", value=64, minimum=64, maximum=1024, interactive=False)
-            threshold_b = gr.Slider(label="Threshold B", value=64, minimum=64, maximum=1024, interactive=False)
-        with gr.Row(visible=False) as vid_path_row:
-            vid_path = gr.Textbox(value='', label="ControlNet Input Video/ Image Path", interactive=True)
-        with gr.Row(visible=False) as mask_vid_path_row:  # invisible temporarily since 26-04-23 until masks are fixed
-            mask_vid_path = gr.Textbox(value='', label="ControlNet Mask Video/ Image Path (*NOT WORKING, kept in UI for CN's devs testing!*)", interactive=True)
-        with gr.Row(visible=False) as control_mode_row:
-            control_mode = gr.Radio(choices=["Balanced", "My prompt is more important", "ControlNet is more important"], value="Balanced", label="Control Mode", interactive=True)
-        with gr.Row(visible=False) as env_row:
-            resize_mode = gr.Radio(choices=["Outer Fit (Shrink to Fit)", "Inner Fit (Scale to Fit)", "Just Resize"], value="Inner Fit (Scale to Fit)", label="Resize Mode", interactive=True)
-        with gr.Row(visible=False) as control_loopback_row:
-            loopback_mode = gr.Checkbox(label="LoopBack mode", value=False, interactive=True)
-        hide_output_list = [pixel_perfect, low_vram, mod_row, weight_row, start_cs_row, end_cs_row, env_row, overwrite_frames, vid_path_row, control_mode_row, mask_vid_path_row,
-                            control_loopback_row]  # add mask_vid_path_row when masks are working again
+        with gr.Row(visible=False) as motion_lora_row:
+            motion_lora_schedule = gr.Textbox(label="Motion lora", lines=1, value='0:("")', interactive=True)
+        with gr.Row(visible=False) as window_row:
+            window_length = gr.Textbox(label="Number of sliding window frames", lines=1, value='0:(16)', interactive=True)
+        with gr.Row(visible=False) as overlap_row:
+            # TODO: expose cadence as a variable
+            window_overlap = gr.Textbox(label="Number of overlapping frames", lines=1, value='0:(15)', interactive=True)
+        with gr.Row(visible=False) as latent_power_row:
+            latent_power = gr.Textbox(label="Latent power schedule", lines=1, value='0:(0.0)', interactive=True)
+        with gr.Row(visible=False) as latent_scale_row:
+            latent_scale = gr.Textbox(label="Latent scale schedule", lines=1, value='0:(1.0)', interactive=True)
+        hide_output_list = [enabled, motion_lora_row, mod_row, window_row, overlap_row, latent_power_row, latent_scale_row]
         for cn_output in hide_output_list:
             enabled.change(fn=hide_ui_by_cn_status, inputs=enabled, outputs=cn_output)
 
-        # hide vid/image input fields
-        loopback_outs = [vid_path_row, mask_vid_path_row]
-        for loopback_output in loopback_outs:
-            loopback_mode.change(fn=hide_file_textboxes, inputs=loopback_mode, outputs=loopback_output)
         infotext_fields.extend([
             (model, f"AnimateDiff Model"),
         ])
